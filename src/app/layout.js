fb2f8a471603a3fe5e04bd0acddeba868e59e251
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-08-28 20:47:06 +0200

    feat: add scss dependency and add meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import { v4 as uuid } from 'uuid'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +12,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta key={uuid()} charset="utf-8" />
+        <meta key={uuid()} http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta key={uuid()} name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta key={uuid()} property="og:title" content="Sanctus Omega Broderskab" />
+        <meta key={uuid()} property="og:image" content="@image('/Omegamai.jpeg')" />
+        <meta key={uuid()} property="og:description" content="Linjeforeningen for Elektronisk Systemdesign og Innovasjon (MTELSYS) og Kybernetikk og Robotikk (MTTK) ved Norges Tekniske-Naturvitenskapelige Universitet (NTNU)" />
+        <meta key={uuid()} property="twitter:title" content="Sanctus Omega Broderskab" />
+        <meta key={uuid()} property="twitter:image" content="@image('/Omegamai.jpeg')" />
+        <meta key={uuid()} property="twitter:description" content="Linjeforeningen for Elektronisk Systemdesign og Innovasjon (MTELSYS) og Kybernetikk og Robotikk (MTTK) ved Norges Tekniske-Naturvitenskapelige Universitet (NTNU)" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
