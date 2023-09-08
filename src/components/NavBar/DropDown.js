a0b32c766bc7e5ecd299ba98b401934ea42e18a3
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-09-08 11:09:18 +0200

    chor: fix key prop to pass tests

--- src/components/NavBar/DropDown.js
+++ src/components/NavBar/DropDown.js
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import styles from "./DropDown.module.scss"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { v4 as uuid } from 'uuid'
 
 function DropDown({ name, items }) {
     return (
@@ -8,7 +9,7 @@ function DropDown({ name, items }) {
             <div className={styles.name}>{ name }</div>
             <div name={name}>
                 {items.map(item => 
-                    <div>
+                    <div key ={uuid()}>
                         <Link href={item.href}>
                             <div>   
                                 <FontAwesomeIcon icon={item.icon} />    
