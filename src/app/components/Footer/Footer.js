ccf2ab64ad74ecbecae1785e8255bca1bbdac94d
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-09-14 18:29:53 +0200

    feat: footer add links and, images, and some styling

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -5,31 +5,37 @@ import styles from './Footer.module.scss'
 
 import addToHomeScreen from '@/images/add_to_home_screen.png'
 import logo from '@/images/omega_logo_white.png'
+import nordic from '@/images/nordic.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faXTwitter
+  faXTwitter,
+  faFacebookSquare,
+  faInstagram
 } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
   return (
     <footer className={styles.Footer}>
       <div className={styles.footerPart}>
-        <Image alt="sct. omega broderskab" src={logo} width={500}/>
-        <h3>Linjeforeningen for Elektronisk Systemdesign og Innovasjon (MTELSYS) og Kybernetikk og Robotikk (MTTK) ved Norges Tekniske-Naturvitenskapelige Universitet (NTNU)</h3>
-        <div class={styles.icons}>
-            <Link href="/infopages/pwa">
-              <Image src={addToHomeScreen} />
+        <Image src={logo} width={300} alt="sct. omega broderskab" />
+        <p>
+          Linjeforeningen for Elektronisk Systemdesign og Innovasjon (MTELSYS) og Kybernetikk og 
+          Robotikk (MTTK) ved Norges Tekniske-Naturvitenskapelige Universitet (NTNU)
+        </p>
+        <div>
+            <Link className={styles.pwa} href="/infopages/pwa">
+              <Image src={addToHomeScreen} width={200} />
             </Link>
-            <div>
+            <div className={styles.icons}>
               <Link href="https://twitter.com/OmegaVevcom" target="_blank">
                 <FontAwesomeIcon icon={faXTwitter} />
               </Link>
-              <Link href="https://www.facebook.com/SctOmegaBroderskab/" target="_blank" class="ml-1">
-                <i class="fab fa-facebook fa-2x"></i>
+              <Link href="https://www.facebook.com/SctOmegaBroderskab/" target="_blank">
+                <FontAwesomeIcon icon={faFacebookSquare} />
               </Link>
-              <Link href="https://www.instagram.com/sctomega/" target="_blank" class="ml-1">
-                <i class="fab fa-instagram fa-2x"></i>
+              <Link href="https://www.instagram.com/sctomega/" target="_blank">
+                <FontAwesomeIcon icon={faInstagram} />
               </Link>
             </div>
         </div>
@@ -49,10 +55,10 @@ function Footer() {
         <p class="mb-0">Elektro-bygget</p>
         <p class="mb-0">7491 Trondheim</p>
       </div>
-      <div class="col-lg-2">
-        <div class="d-flex justify-content-center mt-lg-0 mt-4">
-          <a href="http://www.nordicsemi.com" target="_blank"><img src="@STATIC(/images/nordic.png)" id="img-main-sponsor"/></a>
-        </div>
+      <div className={styles.footerPart}>
+          <Link href="http://www.nordicsemi.com" target="_blank">
+            <Image src={nordic}/>
+          </Link>
       </div>
   </footer>
   )
