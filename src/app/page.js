307626b2ba860de52ab22b811630a1228ed7fb30
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-09-16 20:01:53 +0200

    feat: started work on frontpage sections

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import SocialIcons from './components/SocialIcons/SocialIcons'
 import styles from './page.module.scss'
 
 import logo from '@/images/logo_white.png'
+import kappemann from '@/images/kappemann.jpeg'
 
 export default function Home() {
   return (
@@ -20,9 +21,17 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div>
-        <Image></Image>
-        <span>Om Omega</span>
+      <div className={styles.section}>
+        <Image src={kappemann}/>
+        <h3>Om Omega</h3>
+        <p>
+          Sct. Omega Broderskab ble offisielt stiftet 21. november 1919 av et kull elektrostudenter 
+          som må ha hatt et svært godt kameratskap og sosialt miljø. Til å begynne med var det en 
+          eksklusiv klubb forbeholdt 3. og 4. årskurs, og for å bli tatt opp måtte man sende inn 
+          en fyldig søknad. Dette har endret seg gjennom årene, og i dag blir medlemmene tatt 
+          opp automatisk fra 1. klasse.
+        </p>
+        <Link href="infopages/about">Les mer</Link>
       </div>
     </div>
   )
