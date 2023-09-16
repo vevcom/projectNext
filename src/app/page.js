e2c64ab1fc41287a6e69290c72eb1d4532e91b47
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-09-16 18:53:35 +0200

    feat: logo and front image

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
-import styles from "./page.module.scss"
+import Image from 'next/image'
+import styles from './page.module.scss'
+
+import logo from '@/images/logo_white.png'
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
-      projectnext
+      <div className={styles.frontImage}>
+        <div className={styles.front}>
+          <div>
+            <Image loading="eager" src={logo} width={300}/>
+            <button>Logg in</button>
+            <button>Ny student</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
