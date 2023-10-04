5c964c779bb815807aa9054fb5d941fa45ad2cf3
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-10-04 13:32:56 +0200

    chor: add card layout to the site

--- src/app/user/login/page.js
+++ src/app/user/login/page.js
@@ -1,8 +1,19 @@
 import styles from './page.module.scss'
+import magiskHatt from "@/images/magisk_hatt.png"
+import Image from 'next/image'
 
 function LogIn() {
   return (
-    <div className={styles.wrapper}> log in </div>
+    <div className={styles.wrapper}>
+        <div className={styles.card}>
+            <form>
+                log inn nå!!
+            </form>
+            <div className={styles.image}>
+                <Image width={200 } src={magiskHatt} />
+            </div>
+        </div>
+    </div>
   )
 }
 
