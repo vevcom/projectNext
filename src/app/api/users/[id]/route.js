81f13e9ab32e41a3bbfb29934689e85322c85297
Paulius Juzenas <59730296+Paulijuz@users.noreply.github.com>
2023-10-13 13:09:31 +0200

    feat: basic error handling in api

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -3,7 +3,7 @@ import prisma from "@/prisma"
 export async function GET(request, { params }) {    
     const user = await prisma.user.findUnique({
         where: {
-            id: params.id
+            id: Number(params.id)
         }
     })
     return Response.json(user)
--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,26 +1,39 @@
-import { NextResponse } from "next/server"
+import { Prisma } from '@prisma/client'
 
 import prisma from "@/prisma"
 
 export async function GET() {
     const users = await prisma.user.findMany()
 
-    return NextResponse.json(users)
+    return Response.json(users)
 }
 
 export async function POST(req) {  
     const body = await req.json()
     
-    const { email } = body;
+    const { email, firstname, lastname } = body;
+    
     if (!email) {
-        return NextResponse.json({}, { status: 400 })
+        return Response.json({}, { status: 400 })
     }
 
+    try {
     const user = await prisma.user.create({
         data: {
-            email: email
+            email: email,
+            firstname: firstname,
+            lastname: lastname
         }
     })
 
     return Response.json(user)
+    } catch (error) {  
+        // synes dette er en veldig stygg måte å håndtere feil på
+        if (
+            error instanceof Prisma.PrismaClientKnownRequestError &&
+            error.code === 'P2002'
+        ) {
+            return Response.json({}, { status: 409 })
+        }       
+    }   
 }
