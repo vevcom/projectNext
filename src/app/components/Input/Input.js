68e41dcb8dc6287572410be4c2c65427ec513a9b
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2023-10-14 17:10:58 +0200

    Feat: Add a login form

--- src/app/components/Input/Input.js
+++ src/app/components/Input/Input.js
@@ -1,10 +1,11 @@
+import { v4 as uuid } from 'uuid'
 import styles from "./Input.module.scss"
 
 
 function Input({label, type, id}) {
     label = (label !== undefined) ? label : ''
     type  = (type  !== undefined) ? type  : 'text'
-    id    = (id    !== undefined) ? id    : "id_input_" + Math.random().toString(16).slice(2)
+    id    = (id    !== undefined) ? id    : "id_input_" + uuid()
 
     return <div class={styles.Input}>
         <input type={type} id={id} required/>
--- src/app/user/login/page.js
+++ src/app/user/login/page.js
@@ -1,13 +1,21 @@
+import Input from '@/app/components/Input/Input'
 import styles from './page.module.scss'
 import magiskHatt from "@/images/magisk_hatt.png"
 import Image from 'next/image'
+import PrimaryButton from '@/app/components/PrimaryButton/PrimaryButton'
+import Checkbox from '@/app/components/Checkbox/Checkbox'
 
 function LogIn() {
   return (
     <div className={styles.wrapper}>
         <div className={styles.card}>
-            <form>
-                log inn nå!!
+            <form className={styles.form} action="javasciprt:void(0);">
+                <Input label="E-post"/>
+                <Input label="Passord" type="password"/>
+
+                <Checkbox label="Husk meg"></Checkbox>
+
+                <PrimaryButton text="Logg inn"></PrimaryButton>
             </form>
             <div className={styles.image}>
                 <Image width={200 } src={magiskHatt} />
