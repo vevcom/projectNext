3b135c9e5bf8a7aa48132c2fb8c1d18e2861c101
Paulius Juzenas <59730296+Paulijuz@users.noreply.github.com>
2023-10-20 09:37:16 +0200

    feat: profile test page

---
+++ src/app/users/[username]/page.js
@@ -0,0 +1,36 @@
+import { redirect } from 'next/navigation'
+import { getServerSession } from "next-auth";
+
+import authOptions from '@/auth';
+
+import prisma from "@/prisma"
+
+async function AuthTest({ params }) {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+        redirect('/login')
+    }
+
+    const me = params.username === "me"
+    let username = me ? session.user.username : params.username
+
+    const user = await prisma.user.findUnique({
+        where: {
+            username: username
+        }
+    })
+
+    if (!user) {
+        redirect('/login')
+    }
+
+    return (
+        <>
+            <h1>{`${user.firstname} ${user.lastname}`}</h1>
+            <p>{`E-post: '${user.email}'`}<br/>{`Passord: '${user.password}'`}</p>
+        </>
+    )
+}
+
+export default AuthTest 
