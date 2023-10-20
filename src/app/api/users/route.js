65cae96ac0920303d56b8aeb196c361c377c4b7f
Paulius Juzenas <59730296+Paulijuz@users.noreply.github.com>
2023-10-20 09:36:25 +0200

    fix: add 500 error

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -11,22 +11,24 @@ export async function GET() {
 export async function POST(req) {  
     const body = await req.json()
     
-    const { email, firstname, lastname } = body;
+    const { username, password, email, firstname, lastname } = body;
     
-    if (!email) {
+    if (!username || !password || !email || !firstname || !lastname) {
         return Response.json({}, { status: 400 })
     }
 
     try {
-    const user = await prisma.user.create({
-        data: {
-            email: email,
-            firstname: firstname,
-            lastname: lastname
-        }
-    })
+        const user = await prisma.user.create({
+            data: {
+                username: username,
+                email: email,
+                password: password,
+                firstname: firstname,
+                lastname: lastname
+            }
+        })
 
-    return Response.json(user)
+        return Response.json(user)
     } catch (error) {  
         // synes dette er en veldig stygg måte å håndtere feil på
         if (
@@ -34,6 +36,8 @@ export async function POST(req) {
             error.code === 'P2002'
         ) {
             return Response.json({}, { status: 409 })
-        }       
+        }   
+        
+        return Response.json({}, { status: 500 })
     }   
 }
