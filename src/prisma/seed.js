30821a2e17a806b5db22a358a245542dc147b026
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-10-25 23:20:58 +0200

    feat: add seeding

--- src/prisma/seed.js
+++ src/prisma/seed.js
@@ -1,12 +1,22 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+
+console.log("seed starting...")
 async function main() {
   const harambe = await prisma.user.upsert({
+    where: {
+        email: 'harambe@harambesen.io'
+    },
+    update: {
+
+    },
     create: {
-      email: 'harambe@harambesen.io',
+        firstname: "Harambe",
+        lastname: "Harambesen",
+        email: 'harambe@harambesen.io',
     },
   })
-  console.log({ alice })
+  console.log({ harambe })
 }
 main().then(async () => {
     await prisma.$disconnect()
