0757338c9b0b5d99f4809f222b7c9ddc5f8e4e6b
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-10-28 01:16:57 +0200

    chore: change three last components to tsx

---
+++ src/app/components/YouTube/YouTube.tsx
@@ -0,0 +1,14 @@
+import styles from './YouTube.module.scss'
+
+type PropTypes = {
+	src: string,
+}
+
+function YouTube({src}: PropTypes) {
+
+	return (
+		<iframe allowFullScreen className={styles.YouTube} src={src.replace('/watch?v=', '/embed/')}>YouTube</iframe>
+	)
+}
+
+export default YouTube
