2e963df2c81c49c2a602f17f724f719313ecfeb9
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-10-31 20:30:38 +0100

    chore: install dev dependecies for linting ts

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -2,5 +2,6 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { DefaultArgs     } from "@prisma/client/runtime/library";
 
 declare global {
+   // eslint-disable-next-line no-var
    var prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>
 }
