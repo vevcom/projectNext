abc01825f58c584512269e2df8ee1e902a50391c
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-11-05 18:11:51 +0100

    fix: check for type File in action

--- src/app/actions/images/create.ts
+++ src/app/actions/images/create.ts
@@ -1,33 +1,33 @@
 'use server'
 import { join } from 'path'
-import { writeFile } from "fs/promises"
-import prisma from "@/prisma"
-import { z } from 'zod' 
+import { writeFile } from 'fs/promises'
+import prisma from '@/prisma'
+import { z } from 'zod'
 import { v4 as uuid } from 'uuid'
+import { File } from 'buffer'
 
 export default async function create(rawdata: FormData) {
     const schema = z.object({
-        file: z.instanceof(File),
+        file: z.instanceof(File).refine((file) => file.size < 1024 * 1024, 'File size must be less than 1mb'),
         name: z.string(),
     })
-    let data
-    try {
-        data = schema.parse(rawdata)
-    } catch {
-        return { success: false, error: 'Invalid data'}
-    }
+    const data = schema.parse({
+        file: rawdata.get('file'),
+        name: rawdata.get('name'),
+    })
 
     const bytes = await data.file.arrayBuffer()
     const buffer = Buffer.from(bytes)
-
     const image = await prisma.image.create({
         data: {
             name: data.name,
             fsLocation: uuid(),
         }
+    }).catch(err => {
+        console.log(err)
     })
+    if (!image) return { success: false }
 
-    await writeFile(join('/', 'images', image.fsLocation), buffer)
-
+    await writeFile(join(__dirname, 'images', image.fsLocation), buffer)
     return { success: true }
 }
