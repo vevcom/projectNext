207a758bbe427c756873d04a57f71d203a69912e
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-11-05 17:44:30 +0100

    feat: add zod for validating data in server actions

--- src/app/actions/images/create.ts
+++ src/app/actions/images/create.ts
@@ -2,15 +2,30 @@
 import { join } from 'path'
 import { writeFile } from "fs/promises"
 import prisma from "@/prisma"
-export default async function create(data: FormData) {
-    const file = data.get('file') as File | null
-    if (!file) {
-        throw new Error('no image uploaded')
+import { z } from 'zod' 
+import { v4 as uuid } from 'uuid'
+
+export default async function create(rawdata: FormData) {
+    const schema = z.object({
+        file: z.instanceof(File),
+        name: z.string(),
+    })
+    let data
+    try {
+        data = schema.parse(rawdata)
+    } catch {
+        return { success: false, error: 'Invalid data'}
     }
-    const bytes = await file.arrayBuffer()
+
+    const bytes = await data.file.arrayBuffer()
     const buffer = Buffer.from(bytes)
 
-    const image = await prisma.image.create({})
+    const image = await prisma.image.create({
+        data: {
+            name: data.name,
+            fsLocation: uuid(),
+        }
+    })
 
     await writeFile(join('/', 'images', image.fsLocation), buffer)
 
--- src/app/components/ImageUploader/ImageUploader.tsx
+++ src/app/components/ImageUploader/ImageUploader.tsx
@@ -1,14 +1,11 @@
-'use client'
-
-import { useFormStatus } from 'react-dom'
 import create from '@/app/actions/images/create'
 
 export default function ImageUploader() {
-    const { pending }  = useFormStatus()
     return (
         <form action={create}>
             <input type="file" name="file" />
-            <input type="submit" value="Upload" />
+            <input type="text" name="name" />
+            <button type="submit" value="Upload">Upload</button>
         </form>
     )
 }
