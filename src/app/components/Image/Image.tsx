8049ac28d557535f50982636dd7be458f91ba9ca
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-11-08 12:52:18 +0100

    refactor: use Image component in Sections on frontpage

--- src/app/components/Image/Image.tsx
+++ src/app/components/Image/Image.tsx
@@ -5,28 +5,25 @@ import { default as NextImage, ImageProps } from 'next/image'
 type PropTypes = Omit<ImageProps, 'src' | 'alt'> & {
     name: string,
     width: number,
-    height: number,
 }
 
 export default async function Image({ name, ...props } : PropTypes) {
     const image = await prisma.image.findUnique({
         where: { name }
     })
-    let imagesrc
     try {
-        console.log(`./../../store/images/${image?.fsLocation}`)
-        imagesrc = require(`./../../../../store/images/${image?.fsLocation}`);
-    } catch (err) {
-        imagesrc = require(`./../../../../public/default_image.jpeg`)
-    }
-
-    return (
-        imagesrc ? (
-            <div>
-                <NextImage alt={image?.alt || 'default image'} src={imagesrc} {...props} />
-            </div>
-        ) : (
-            <div>error</div>
+        if (!image) throw `no image found with name: ${name}`
+        const imagesrc = require(`./../../../../store/images/${image?.fsLocation}`)
+        return (
+            <NextImage alt={image.alt} src={imagesrc} {...props} />
         )
-    )
+    } catch (err) {
+        const imagesrc = require(`./../../../../public/default_image.jpeg`)
+        return (
+            <div>
+                <div>Could not find image {name}</div>
+                <NextImage alt={'default image'} src={imagesrc} {...props} />
+            </div>
+        )
+    }
 }
