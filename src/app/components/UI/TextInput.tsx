ab9665c096a191e830819607e902cfd623520c6d
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-11-09 00:45:05 +0100

    fix: TextInpot should accept tupe password

--- src/app/components/UI/TextInput.tsx
+++ src/app/components/UI/TextInput.tsx
@@ -4,15 +4,17 @@ import { InputHTMLAttributes } from 'react'
 import styles from './TextInput.module.scss'
 
 type PropTypes = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> & {
-    label: string
+    label: string,
+    type?: 'text' | 'password',
 }
 
 export default function TextInput({ label = 'default', ...props } : PropTypes) {
     props.id ??= `id_input_${uuid()}`
+    props.type ??= 'text'
 
     return (
         <div className={styles.Input}>
-            <input type="text" {...props}/>
+            <input {...props}/>
             <label htmlFor={props.id}>{label}</label>
         </div>
     )
