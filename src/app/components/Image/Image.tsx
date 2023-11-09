537f35c607bd098330cdd14ac3e19a84648cfb2a
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-11-09 16:37:39 +0100

    chore: add wrapperto Image component

--- src/app/components/Image/Image.tsx
+++ src/app/components/Image/Image.tsx
@@ -1,5 +1,6 @@
 import prisma from '@/prisma'
 import { default as NextImage, ImageProps } from 'next/image'
+import styles from './Image.module.scss'
 
 type PropTypes = Omit<ImageProps, 'src' | 'alt'> & {
     name: string,
@@ -15,7 +16,9 @@ export default async function Image({ alt, name, ...props } : PropTypes) {
         if (!image) throw `no image found with name: ${name}`
         const imagesrc = await import(`./../../../../store/images/${image?.fsLocation}`)
         return (
+        <div className={styles.Image}>
             <NextImage alt={alt ?? image.alt} src={imagesrc} {...props} />
+        </div>
         )
     } catch (err) {
         const imagesrc = await import('./../../../../store/images/default_image.jpeg')
