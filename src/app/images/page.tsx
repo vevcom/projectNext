27a67c43553e32e67ed264c59160879934514bb0
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-12-21 20:23:28 +0100

    chore: all collections displays in /images

--- src/app/images/page.tsx
+++ src/app/images/page.tsx
@@ -1,19 +1,24 @@
 import ImageUploader from '@/components/ImageUploader/ImageUploader'
 import prisma from '@/prisma'
+import styles from './page.module.scss'
 
 export default async function Images() {
     const collections = await prisma.imageCollection.findMany()
     return (
         <>
             <ImageUploader />
-            <h3>Hvad der har blivet fotografert</h3>
-            {
-                collections.map((collection) => (
-                    <div key={collection.id}>
-                        <h4>{collection.name}</h4>            
-                    </div>
-                ))
-            }
+            <div className={styles.wrapper}>
+                <h3>Hvad der har blivet fotografert</h3>
+                <span className={styles.collections}>
+                {
+                    collections.map((collection) => (
+                        <div className={styles.collection} key={collection.id}>
+                            <h4>{collection.name}</h4>            
+                        </div>
+                    ))
+                }
+                </span>
+            </div>
         </>
     )
 }
