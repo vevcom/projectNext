bdc0f712f9fc236e75b5f0a4fe593ffd31357a96
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-12-27 23:17:50 +0100

    feat: create update action for image collection

--- src/actions/images/collections/update.ts
+++ src/actions/images/collections/update.ts
@@ -1,1 +1,35 @@
 'use server'
+import { z } from 'zod'
+import prisma from '@/prisma'
+import errorHandeler from '@/prisma/errorHandler'
+import type { ActionReturn } from '@/actions/type'
+import { ImageCollection } from '@prisma/client'
+
+export default async function create(collectionId: number, rawdata: FormData): Promise<ActionReturn<ImageCollection>> {
+    const schema = z.object({
+        name: z.string().max(40).min(2).trim().optional(),
+        description: z.string().max(500).min(2).trim().optional(),
+    })
+    const parse = schema.safeParse({
+        name: rawdata.get('name'),
+        description: rawdata.get('description'),
+    })
+
+    if (!parse.success) {
+        return { success: false, error: parse.error.issues }
+    }
+
+    const data = parse.data
+
+    try {
+        const collection = await prisma.imageCollection.update({
+            where: {
+                id: collectionId,
+            },
+            data,
+        })
+        return { success: true, data: collection }
+    } catch (error) {
+        return errorHandeler(error)
+    }
+}
--- src/app/images/collections/[id]/CollectionAdmin.tsx
+++ src/app/images/collections/[id]/CollectionAdmin.tsx
@@ -1,4 +1,5 @@
 import create from '@/actions/images/create'
+import update from '@/actions/images/collections/update'
 import Form from '@/app/components/Form/Form'
 import FileInput from '@/app/components/UI/FileInput'
 import TextInput from '@/app/components/UI/TextInput'
@@ -16,11 +17,10 @@ export default function CollectionAdmin({collectionId}: PropTypes) {
                 <TextInput color="black" label="alt" name="alt" />
                 <FileInput label="file" name="file" color="primary" />
             </Form>
-            <Form title='Edit collection' createText='update' action={create.bind(null, collectionId)}>
+            <Form title='Edit collection' createText='update' action={update.bind(null, collectionId)}>
                 <TextInput color="black" label="collection name" name="name" />
-                <TextInput color="black" label="description" name="name" />
+                <TextInput color="black" label="description" name="description" />
             </Form>
-        </div>
-        
+        </div> 
     )
 }
