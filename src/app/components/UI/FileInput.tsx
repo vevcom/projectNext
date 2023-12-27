9520eee531ea901d7c5e9fa8aa0f25b8851080e4
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-12-27 17:02:45 +0100

    chore: fix styling of FileInput

--- src/app/components/UI/FileInput.tsx
+++ src/app/components/UI/FileInput.tsx
@@ -1,5 +1,8 @@
+'use client'
+
+import { useState } from 'react';
 import { v4 as uuid } from 'uuid'
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, ChangeEvent } from 'react'
 
 import styles from './FileInput.module.scss'
 
@@ -10,11 +13,19 @@ type PropTypes = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> & {
 
 export default function FileInput({ label = 'default', color, ...props } : PropTypes) {
     props.id ??= `id_input_${uuid()}`
-    color ??= 'primary'
+    color ??= 'black'
+
+    const [fileName, setFileName] = useState('')
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) setFileName(e.target.files[0].name)
+    }
+    
     return (
         <div id={props.name} className={`${styles.FileInput} ${styles[color]}`}>
-            <input {...props} type="file" className={styles.field} />
-            <label className={styles.label}>{label}</label>
+            <input {...props} type="file" name={props.name} className={styles.field} onChange={handleFileChange} />
+            <label htmlFor={props.id} className={styles.label}>{label}</label>
+            <div className={styles.fileName}>{fileName}</div>
         </div>
     )
 }
