bd5896f121418439b087d0e4b3ae1892f767ee7f
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-12-27 23:04:32 +0100

    chore: finalizes destop layout for image/collection

--- src/app/images/collections/[id]/CollectionAdmin.tsx
+++ src/app/images/collections/[id]/CollectionAdmin.tsx
@@ -11,13 +11,14 @@ type PropTypes = {
 export default function CollectionAdmin({collectionId}: PropTypes) {
     return (
         <div className={styles.CollectionAdmin}>
-            <Form title='upload image' createText='upload' action={create.bind(null, collectionId)}>
-                <FileInput label="file" name="file" color="primary" />
+            <Form title='Upload image' createText='upload' action={create.bind(null, collectionId)}>
                 <TextInput color="black" label="name" name="name" />
                 <TextInput color="black" label="alt" name="alt" />
+                <FileInput label="file" name="file" color="primary" />
             </Form>
             <Form title='Edit collection' createText='update' action={create.bind(null, collectionId)}>
-                hei
+                <TextInput color="black" label="collection name" name="name" />
+                <TextInput color="black" label="description" name="name" />
             </Form>
         </div>
         
