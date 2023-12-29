b8b080dd46a5d2c8f8217b1c4c1d202be6d96c23
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-12-29 17:52:48 +0100

    feat: adds Dropzone component

---
+++ src/app/components/UI/Dropzone.tsx
@@ -0,0 +1,29 @@
+import React, { useCallback, useState } from 'react'
+import styles from './Dropzone.module.scss'
+
+const Dropzone = () => {
+    const [files, setFiles] = useState<File[]>([]);
+
+    const onDrop = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault()
+        const droppedFiles = Array.from(event.dataTransfer.files)
+        setFiles(prev => [...prev, ...droppedFiles])
+    }, []);
+
+    const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault()
+    }
+
+    return (
+        <div onDrop={onDrop} onDragOver={onDragOver} className={styles.Dropzone}>
+            Drop some files here!
+            <ul>
+                {files.map((file, index) => (
+                    <li key={index}>{file.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Dropzone;
