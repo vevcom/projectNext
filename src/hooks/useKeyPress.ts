b4838d35003212a58186258bc2f2aca69113f664
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-12-30 17:21:31 +0100

    feat: adds Keyboard hook for reacting to key presses

---
+++ src/hooks/useKeyPress.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+function useKeyPress(targetKey: string, callback: (event: KeyboardEvent) => void) {
+    const [keyPressed, setKeyPressed] = useState(false);
+
+    function downHandler(event: KeyboardEvent) {
+        callback(event)
+        const { key } = event;
+        if (key === targetKey) {
+            setKeyPressed(true);
+        }
+    }
+
+    const upHandler = ({ key }: KeyboardEvent) => {
+        if (key === targetKey) {
+            setKeyPressed(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', downHandler)
+        window.addEventListener('keyup', upHandler)
+
+        return () => {
+            window.removeEventListener('keydown', downHandler)
+            window.removeEventListener('keyup', upHandler)
+        }
+    }, [])
+
+    return keyPressed;
+}
+
+export default useKeyPress;
