e74032c3643cae1ebbdfb6e82121876c4549d175
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-12-30 13:07:56 +0100

    chore: databinding between input and state

--- src/app/components/UI/Dropzone.tsx
+++ src/app/components/UI/Dropzone.tsx
@@ -1,33 +1,53 @@
 'use client'
 
-import { InputHTMLAttributes, useCallback, useState, useRef } from 'react'
+import { 
+    InputHTMLAttributes, 
+    useCallback, 
+    useState, 
+    ChangeEvent, 
+    DragEvent, 
+    useEffect,
+    useRef,
+} from 'react'
 import styles from './Dropzone.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUpload } from '@fortawesome/free-solid-svg-icons'
 
-type PropTypes = Omit<InputHTMLAttributes<HTMLInputElement>, 'type' | 'name'> & {
+type PropTypes = Omit<InputHTMLAttributes<HTMLInputElement>, 'type' | 'name' | 'multiple'> & {
     label: string,
     color?: 'primary' | 'secondary' | 'red' | 'black',
     name: string,
 }
 
-const Dropzone = ({...props}:PropTypes) => {
-    const [files, setFiles] = useState<File[]>([]);
-    const input = useRef<HTMLInputElement>(null);
+const Dropzone = ({label, color, name, ...props } : PropTypes) => {
+    const [files, setFiles] = useState<File[]>([])
+    const input = useRef<HTMLInputElement>(null)
 
-    const onDrop = useCallback((event: React.DragEvent<HTMLDivElement>) => {
-        event.preventDefault()
-        const droppedFiles = Array.from(event.dataTransfer.files)
-        setFiles(prev => [...prev, ...droppedFiles])
+    color ??= 'black'
+
+    const onDrop = useCallback((event: DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        const droppedFiles = Array.from(event.dataTransfer.files) as File[];
+        setFiles(prev => [...prev, ...droppedFiles]);
+
+        if (input.current) {
+            const dataTransfer = new DataTransfer();
+            droppedFiles.forEach(file => dataTransfer.items.add(file));
+            input.current.files = dataTransfer.files;
+        }
     }, []);
-
-    const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    const filesUpdated = useCallback((event: ChangeEvent<HTMLInputElement>) => {
         event.preventDefault()
-    }
+        const newFiles = Array.from(event.target.files ?? [])
+        setFiles(prev => [...prev, ...newFiles])
+    }, [])
+
+    const onDragOver = (event: DragEvent<HTMLDivElement>) => event.preventDefault()
 
     return (
         <div onDrop={onDrop} onDragOver={onDragOver} className={styles.Dropzone}>
-            <input ref={input} type="file" multiple />
+            <input name={name} onChange={filesUpdated} type="file" multiple {...props} />
+            <p>{label}</p>
             <FontAwesomeIcon icon={faUpload} />
             <ul>
                 {files.map((file, index) => (
