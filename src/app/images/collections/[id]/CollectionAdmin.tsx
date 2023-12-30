3c6d78525d59f1382fb16ff8a527f371c606573d
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-12-30 14:08:07 +0100

    chore: adds final styling of upload many button

--- src/app/images/collections/[id]/CollectionAdmin.tsx
+++ src/app/images/collections/[id]/CollectionAdmin.tsx
@@ -6,6 +6,8 @@ import TextInput from '@/app/components/UI/TextInput'
 import styles from './CollectionAdmin.module.scss'
 import Dropzone from '@/app/components/UI/Dropzone'
 import PopUp from '@/app/components/PopUp/PopUp'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUpload } from '@fortawesome/free-solid-svg-icons'
 
 type PropTypes = {
     collectionId: number
@@ -14,13 +16,18 @@ type PropTypes = {
 export default function CollectionAdmin({ collectionId }: PropTypes) {
     return (
         <div className={styles.CollectionAdmin}>
-            <div>
+            <div className={styles.upload}>
                 <Form title="Upload image" createText="upload" action={create.bind(null, collectionId)}>
                     <TextInput color="black" label="name" name="name" />
                     <TextInput color="black" label="alt" name="alt" />
                     <FileInput label="file" name="file" color="primary" />
                 </Form>
-                <PopUp showButtonContent={<>Upload many</>}>
+                <PopUp showButtonContent={
+                    <>
+                        Upload many
+                        <FontAwesomeIcon icon={faUpload} />
+                    </>
+                }>
                     <Form title="Upload multiple" createText="upload" action={create.bind(null, collectionId)}>
                         <Dropzone label="upload images" name="images"/>
                     </Form>
