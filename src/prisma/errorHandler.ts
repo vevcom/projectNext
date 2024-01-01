2609cbf6d63d053e0142dca42f4a67ea456705e5
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-01 20:53:38 +0100

    fix: change schema to cascade delete images when collection is deleted

--- src/prisma/errorHandler.ts
+++ src/prisma/errorHandler.ts
@@ -5,5 +5,8 @@ export default function errorHandeler(err: unknown) : ActionReturn<never> {
     if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
         return { success: false, error: [{ message: 'Duplicate entry' }] }
     }
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2025') {
+        return { success: false, error: [{ message: 'Record not found' }] }
+    }
     return { success: false, error: [{ message: 'unknown error' }] }
 }
