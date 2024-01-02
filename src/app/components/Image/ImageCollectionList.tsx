91b6a00f70fb04875237a1c4a169506a08331100
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-02 17:07:38 +0100

    chore: loading of new iages is now much better

--- src/app/components/Image/ImageCollectionList.tsx
+++ src/app/components/Image/ImageCollectionList.tsx
@@ -5,7 +5,13 @@ import PopUp from '@/app/components/PopUp/PopUp'
 import ImageCollectionDisplay from '@/app/components/Image/ImageCollectionDisplay'
 import Button from '@/app/components/UI/Button'
 import { default as ImageComponent } from '@/components/Image/Image'
-import { useEffect, useState, useCallback } from 'react'
+import { 
+    useEffect, 
+    useState, 
+    useMemo,
+    useCallback, 
+    Suspense,
+} from 'react'
 import { useInView } from 'react-intersection-observer'
 import read from '@/actions/images/collections/read'
 import { set } from 'zod'
@@ -22,7 +28,6 @@ export default function ImageCollectionList({collection, pageSize}: PropTypes) {
     const [images, setImages] =  useState<Image[]>(collection.images)
     const [page, setPage] = useState(0)
     const [ref, inView] = useInView()
-    const [loading, setLoading] = useState(false)
     const [allLoaded, setAllLoaded] = useState(false)
 
     const loadMoreImages = useCallback(async () => {
@@ -40,29 +45,47 @@ export default function ImageCollectionList({collection, pageSize}: PropTypes) {
         }
     }, [inView])
 
+    const imageComponents = useMemo(() => 
+        images.map(image => <ImageWithFallback key={image.id} image={image} collection={collection} />),
+    [images, collection])
+
     return (
         <div className={styles.ImageCollectionList}>
-            {
-                images.map(image =>
-                    <div key={image.id} className={styles.imageAndBtn}>
-                        <ImageComponent width={200} image={image} />
-                        <PopUp showButtonContent={<></>}>
-                            <ImageCollectionDisplay 
-                                startImageName={image.name} 
-                                collection={collection} 
-                            />
-                        </PopUp>
-                    </div>
-                )
-            }
+            {imageComponents}
             <span className={styles.loadingControl}>
-            {
+            <Suspense fallback={<div>Loading...</div>}>
+                {
                 allLoaded ? (
                     <i>No more images to load</i>
                 ) : 
                     <Button onClick={loadMoreImages}>Load more</Button>
-            }
+                }       
+            </Suspense>
             </span>
+        </div>
+    )
+}
+
+function ImageWithFallback({ 
+    image, 
+    collection 
+}: { 
+    image: Image, 
+    collection: ImageCollection & {
+        images: Image[],
+    },
+}) {
+    return (
+        <div className={styles.imageAndBtn}>
+            <Suspense fallback={<div className={styles.skeleton}></div>}>
+                <ImageComponent width={200} image={image} />
+            </Suspense>
+            <PopUp showButtonContent={<></>}>
+                <ImageCollectionDisplay 
+                    startImageName={image.name} 
+                    collection={collection} 
+                />
+            </PopUp>
         </div>
     )
 }
