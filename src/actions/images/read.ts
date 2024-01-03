6bb24df9d843620583ff9647bf3813fcc594bdf8
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-03 16:25:37 +0100

    chore: adds action to read one image for name based on name

--- src/actions/images/read.ts
+++ src/actions/images/read.ts
@@ -2,6 +2,7 @@
 import prisma from "@/prisma"
 import type { ImageCollection, Image } from "@prisma/client"
 import type { ActionReturn, ReadPageInput } from "@/actions/type"
+import errorHandeler from "@/prisma/errorHandler"
 
 export async function readPage<const PageSize extends number>({page, details}: ReadPageInput<PageSize, {id: number}>)
     : Promise<ActionReturn<ImageCollection & {images: Image[]}>> {
@@ -23,4 +24,18 @@ export async function readPage<const PageSize extends number>({page, details}: R
     })
     if (!collection) return { success: false, error: [{ message: 'Image not found' }] }
     return { success: true, data: collection }
+}
+
+export default async function read(name: string) : Promise<ActionReturn<Image>> {
+    try {
+        const image = await prisma.image.findUnique({
+            where: {
+                name: name,
+            },
+        })
+        if (!image) return { success: false, error: [{ message: 'Image not found' }] }
+        return { success: true, data: image }
+    } catch (error) {
+        return errorHandeler(error)
+    }
 }
