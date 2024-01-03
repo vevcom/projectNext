aadef46310f489f347a280e35e04fad4c7a71e73
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-03 18:40:39 +0100

    chore: adds some abstract type nonsense to render a page through component

--- src/app/components/PageingWrappes/EndlessScroll.tsx
+++ src/app/components/PageingWrappes/EndlessScroll.tsx
@@ -3,13 +3,20 @@ import { useContext, useEffect } from 'react'
 import type { PagingContextType } from '@/context/paging/PagingGenerator'
 import { useInView } from 'react-intersection-observer'
 import Button from '@/components/UI/Button'
+import styles from './EndlessScroll.module.scss'
+import RenderPageData from './RenderPageData'
 
+type PropTypes<Data, PageSize extends number, FetcherDetails> = {
+    pageingContext: PagingContextType<Data, PageSize, FetcherDetails>,
+    details: FetcherDetails,
+    renderer: (data: Data) => JSX.Element,
+}
 
 export default function EndlessScroll<Data, const PageSize extends number, FetcherDetails>
-(pageingContext: PagingContextType<Data, PageSize, FetcherDetails>, details: FetcherDetails) {
+({ pageingContext, details, renderer }: PropTypes<Data, PageSize, FetcherDetails>) {
     const context = useContext(pageingContext)
 
-    //This component must be rendered inside a ImagePagingContextProvider
+    //This component must be rendered inside ContextProvider
     if (!context) throw new Error('No context')
     const [ref, inView] = useInView({
         threshold: 0,
@@ -17,20 +24,24 @@ export default function EndlessScroll<Data, const PageSize extends number, Fetch
 
     useEffect(() => {
         if (inView) {
-            context?.loadMore(details)
+            context.loadMore(details)
         }
     }, [inView])
 
+
     return (
-        <div ref={ref}>
-            {
-                context.state.allLoaded ? (
-                    <i>No more images to load</i>
-                ) : 
-                    <div ref={ref}>
-                        <Button onClick={() => context?.loadMore(details)}>Load more</Button>
-                    </div>
-            }
+        <div className={styles.EndlessScroll}>
+            <RenderPageData data={context.state.data} renderer={renderer} />
+            <span className={styles.loadingControl} ref={ref}>
+                {
+                    context.state.allLoaded ? (
+                        <i>No more images to load</i>
+                    ) : 
+                        <div ref={ref}>
+                            <Button onClick={() => context?.loadMore(details)}>Load more</Button>
+                        </div>
+                }
+            </span>
         </div>
     )
 }
---
+++ src/app/components/PageingWrappes/RenderPageData.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+type PropTypes<Data> = {
+    data: Data[],
+    renderer: (data: Data) => JSX.Element,
+}
+
+export default function RenderPageData<Data>({ data, renderer }: PropTypes<Data>) {
+    return data.map(renderer)
+}
