8c3873254ff6a044dfdc98c93f8a1b6e4b9b3b97
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-03 16:41:51 +0100

    feat: error boundary now works, sort off?

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,13 +2,14 @@
 
 import Button from './components/UI/Button'
 import styles from './error.module.scss'
-import Image from './components/Image/Image'
+import ImageLink from './components/Image/ImageLink'
 
 export default function ErrorBoundary({error, reset} : {error: Error, reset: () => void}) {
     return (
         <div className={styles.wrapper}>
             <div className={styles.info}>
                 <div className={styles.imageContainer}>
+                    <ImageLink name="logo_simple" width={60} />
                 </div>
                 <h3>500 - {error.message}</h3>
             </div>
