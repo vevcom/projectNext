ec053d035b6575b7e393d111ad0488922de5bdee
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-06 14:42:25 +0100

    chore: two way databinding on EditModeSwitch

--- src/app/components/EditModeSwitch/EditModeSwitch.tsx
+++ src/app/components/EditModeSwitch/EditModeSwitch.tsx
@@ -1,11 +1,12 @@
 'use client'
-import { useContext } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { EditModeContext } from '@/context/EditMode'
 import styles from './EditModeSwitch.module.scss'
 import type { ChangeEvent } from 'react'
 
 export default function EditModeSwitch() {
     const editingContext = useContext(EditModeContext)
+    const ref = useRef<HTMLInputElement>(null)
     if (!editingContext) throw new Error('No EditModeContext found')
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -13,10 +14,15 @@ export default function EditModeSwitch() {
         editingContext.setEditMode(e.target.checked)
     }
 
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.checked = editingContext.editMode
+        }
+    }, [editingContext.editMode])
+
     return (
         <div className={styles.EditModeSwitch}>
-            <input type="checkbox" id="editModeSwitch" onChange={handleChange} />
-            <label htmlFor="editModeSwitch">Redigeringsmodus</label>
+            <input ref={ref} type="checkbox" id="editModeSwitch" onChange={handleChange} />
         </div>
     )
 }
