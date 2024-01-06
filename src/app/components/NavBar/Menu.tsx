e8283e7486c380de5e7cbdf64efa1274ef5ec2d8
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-06 23:06:00 +0100

    chore: adds basic styling to Menu

--- src/app/components/NavBar/Menu.tsx
+++ src/app/components/NavBar/Menu.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react'
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
 import styles from './Menu.module.scss'
 
@@ -20,24 +20,27 @@ export default function Menu({ items, openBtnContent } : PropTypes) {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
-        isOpen ? (
-            <div className={styles.menu}>
-                <FontAwesomeIcon icon={faTimes} onClick={() => setIsOpen(false)}/>
-                <ul>
-                    {items.map((item) => (
-                        <li key={item.name}>
-                            <Link href={item.href}>
-                                <FontAwesomeIcon icon={item.icon}/>
-                                {item.name}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        ) : (
+        <> 
+        {
+            isOpen ? (
+                <div className={styles.Menu}>
+                    <FontAwesomeIcon icon={faTimes} onClick={() => setIsOpen(false)}/>
+                    <ul>
+                        {items.map((item) => (
+                            <li key={item.name}>
+                                <Link href={item.href}>
+                                    <FontAwesomeIcon icon={item.icon}/>
+                                    {item.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ) : null
+        }
             <button className={styles.openBtn} onClick={() => setIsOpen(true)}>
                 {openBtnContent}
             </button>
-        )
+        </>
     )
 }
