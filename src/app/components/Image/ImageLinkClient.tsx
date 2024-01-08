b67eccd051b0937800a4c463d2c448a8491dc63b
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-08 21:14:30 +0100

    chore:(not functional) changes the way to set Image in ImageLinkClient

--- src/app/components/Image/ImageLinkClient.tsx
+++ src/app/components/Image/ImageLinkClient.tsx
@@ -1,17 +1,20 @@
 'use client'
 import { useState, useEffect } from 'react'
-import type { Image as ImageT } from '@prisma/client'
+import type { Image as ImageT, ImageLink } from '@prisma/client'
 import read from '@/actions/images/links/read'
 import Image from './Image'
 import type { PropTypes } from './ImageLink'
 
 export default function ImageLinkClient({ name, width, alt, ...props }: PropTypes) {
-    const [image, setImage] = useState<ImageT | null>()
+    const [imageLink, setImageLink] = useState<
+        ImageLink & {
+            image: ImageT | null
+        } | null>(null)
     useEffect(() => {
         read(name).then(({ success, data }) => {
-            if (success && data) return setImage(data.image)
+            if (success && data) return setImageLink(data)
             return read('default_image').then(({ success: defaultSuccess, data: defaultImage }) => {
-                if (defaultSuccess && defaultImage) return setImage(defaultImage.image)
+                if (defaultSuccess && defaultImage) return setImageLink(defaultImage)
                 throw new Error('No default image found. To fix add a image called: default_image')
             })
         })
