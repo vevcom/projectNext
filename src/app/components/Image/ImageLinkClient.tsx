f4671d75104e81ee14b2bca2746be4ef88bfc168
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-08 18:41:43 +0100

    chore: renames link to libks

--- src/app/components/Image/ImageLinkClient.tsx
+++ src/app/components/Image/ImageLinkClient.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useEffect } from 'react'
 import type { Image as ImageT } from '@prisma/client'
-import read from '@/actions/images/link/read'
+import read from '@/actions/images/links/read'
 import Image from './Image'
 import type { PropTypes } from './ImageLink'
 
@@ -9,7 +9,7 @@ export default function ImageLinkClient({ name, width, alt, ...props }: PropType
     const [image, setImage] = useState<ImageT | null>()
     useEffect(() => {
         read(name).then(({ success, data }) => {
-            if (success && data) return setImage(data)
+            if (success && data) return setImage(data.image)
             return read('default_image').then(({ success: defaultSuccess, data: defaultImage }) => {
                 if (defaultSuccess && defaultImage) return setImage(defaultImage)
                 throw new Error('No default image found. To fix add a image called: default_image')
