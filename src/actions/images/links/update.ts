563211ce42ebd26ab01f43347b94283ff2fb7e45
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-10 21:22:50 +0100

    feat: creates server action for changeing image in imagelink

---
+++ src/actions/images/links/update.ts
@@ -0,0 +1,26 @@
+'use server'
+import { ActionReturn } from '@/actions/type'
+import { ImageLink } from "@prisma/client"
+import prisma from '@/prisma'
+import errorHandeler from '@/prisma/errorHandler' 
+
+export default async function update(linkId: number, imageId: number) : Promise<ActionReturn<ImageLink>> {
+    try {
+        const imageLink = await prisma.imageLink.update({
+            where: {
+                id: linkId,
+            },
+            data: {
+                image: {
+                    connect: {
+                        id: imageId,
+                    }
+                }
+            }
+        })
+        return { success: true, data: imageLink }
+    } catch (error) {
+        return errorHandeler(error)
+    }
+   
+}
