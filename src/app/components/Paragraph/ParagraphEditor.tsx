ff081e00994574d5a87c482954645f7983931853
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-14 21:55:54 +0100

    chore: adds textarea to edit paragraph

--- src/app/components/Paragraph/ParagraphEditor.tsx
+++ src/app/components/Paragraph/ParagraphEditor.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { EditModeContext } from '@/context/EditMode';
 import styles from './ParagraphEditor.module.scss';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import type { Paragraph } from '@prisma/client';
+import { ChangeEvent } from 'react';
 
 type PropTypes = {
     paragraph: Paragraph
@@ -11,15 +12,20 @@ type PropTypes = {
 export default function ParagraphEditor({paragraph}: PropTypes) {
     const editmode = useContext(EditModeContext)
     if (!editmode) return null
+    const [content, setContent] = useState(paragraph.content)
+
+    const handleContentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        e.preventDefault()
+        setContent(e.target.value)
+    }
 
     return (
-        editmode.editMode ? (
+        editmode.editMode && (
             <div className={styles.ParagraphEditor}>
-                EditParagraph
+                <textarea onChange={handleContentChange}>
+                    {content}
+                </textarea>
             </div>
-        ) : (
-            null
         )
-        
     )
 }
