ec0a683602b0f41fadcb49165c53e1358e8a207e
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-14 18:40:36 +0100

    chore: adds boilerplate for Paragrapg component and actions

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Paragraph from '../components/Paragraph/Paragraph'
+
+export default function Articles() {
+  return (
+    <main>
+        <Paragraph />
+    </main>
+  )
+}
---
+++ src/app/components/Paragraph/Paragraph.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default async function Paragraph() {
+
+    return (
+        <div>Paragraph</div>
+    )
+}
