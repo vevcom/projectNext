e6ce93ba996543d8bea5d41973a9faf1fed66550
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-14 20:15:37 +0100

    chore: passes paragraph

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -3,10 +3,14 @@ import Paragraph from '../components/Paragraph/Paragraph'
 import read from '@/actions/paragraphs/read'
 
 export default async function Articles() {
-    const para = await read('my_first_paragraph')
+    const {success, data: paragraph, error} = await read('my_first_paragraph')
+
+    //should never happen as default behaviour is to create if not found.
+    if (!success || !paragraph) throw new Error(error ? error[0].message : 'something unexpected happen in loading paragraph') 
+    
     return (
         <main>
-            <Paragraph paragraph={para.data || null} />
+            <Paragraph paragraph={paragraph} />
         </main>
     )
 }
--- src/app/components/Paragraph/Paragraph.tsx
+++ src/app/components/Paragraph/Paragraph.tsx
@@ -2,12 +2,11 @@ import type { Paragraph } from '@prisma/client'
 import React from 'react'
 
 type PropTypes = {
-    paragraph: Paragraph | null
+    paragraph: Paragraph
 }
 
 export default async function Paragraph({paragraph}: PropTypes) {
-
     return (
-        <div>{paragraph ? paragraph.content : <i>no content</i>}</div>
+        <div>{paragraph.content ? paragraph.content : <i>no content</i>}</div>
     )
 }
