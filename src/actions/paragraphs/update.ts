df441765c596f71e6819dac30462e7714e1311e7
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-15 11:10:02 +0100

    chore: tries to fix ramark-unlink

--- src/actions/paragraphs/update.ts
+++ src/actions/paragraphs/update.ts
@@ -8,15 +8,18 @@ import rehypeFormat from 'rehype-format'
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
+import remarkUnlink from 'remark-unlink'
 
 export default async function update(id: number, content: string) : Promise<ActionReturn<Paragraph>> {
     //This function expects to get valid md
     try {
         const html = unified()
+            .use(remarkUnlink)
             .use(remarkParse)
             .use(remarkRehype)
             .use(rehypeFormat)
-            .use(rehypeStringify).processSync(content)
+            .use(rehypeStringify)
+            .processSync(content)
         console.log(html)
     } catch (e) {
         console.log(e)
