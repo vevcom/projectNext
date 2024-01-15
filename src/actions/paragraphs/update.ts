069fe20ef8c0dba5c1dc75f08f421a1dc734d2c5
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-15 10:48:52 +0100

    chore: adds more pacages for rehype

--- src/actions/paragraphs/update.ts
+++ src/actions/paragraphs/update.ts
@@ -3,8 +3,25 @@
 import { Paragraph } from "@prisma/client";
 import { ActionReturn } from "../type";
 import errorHandeler from "@/prisma/errorHandler";
+import { unified } from 'unified'
+import rehypeFormat from 'rehype-format'
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
 
 export default async function update(id: number, content: string) : Promise<ActionReturn<Paragraph>> {
+    //This function expects to get valid md
+    try {
+        const html = unified()
+            .use(remarkParse)
+            .use(remarkRehype)
+            .use(rehypeFormat)
+            .use(rehypeStringify).processSync(content)
+        console.log(html)
+    } catch (e) {
+        console.log(e)
+    }
+    
     try {
         const paragraph = await prisma.paragraph.update({
             where: { 
