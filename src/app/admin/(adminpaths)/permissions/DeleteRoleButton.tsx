712753920f2d6f82d0ddd3c09321bae3c8380f08
Paulius Juzenas <59730296+Paulijuz@users.noreply.github.com>
2024-01-16 16:34:08 +0100

    add: delete role button

---
+++ src/app/admin/(adminpaths)/permissions/DeleteRoleButton.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { deleteRole } from "@/actions/permissions"
+import Button from "@/app/components/UI/Button"
+import { useRouter } from 'next/navigation'
+
+type PropType = {
+    roleId: number
+}
+
+export default function DeleteRoleButton({roleId}: PropType) {
+    const router = useRouter()
+
+    async function handleClick() {
+        deleteRole(roleId)
+        router.refresh()
+    }
+
+    return <Button type="button" onClick={handleClick}>Slett</Button>
+}
--- src/app/admin/(adminpaths)/permissions/page.tsx
+++ src/app/admin/(adminpaths)/permissions/page.tsx
@@ -1,8 +1,9 @@
 import styles from './page.module.scss'
 import CreateRoleForm from './CreateRoleForm'
 
-import type { Role } from '@prisma/client'
 import { getRoles } from '@/actions/permissions'
+import DeleteRoleButton from './DeleteRoleButton'
+import TextInput from '@/app/components/UI/TextInput'
 
 export default async function Permissions() {
     const roles = await getRoles()
@@ -18,6 +19,7 @@ export default async function Permissions() {
     const tableContents = Array.from(roles, role => { 
         return {
             name: role.name, 
+            roleId: role.id,
             permissions: [
                 role.useMoney,
                 role.useFridge,
@@ -33,16 +35,18 @@ export default async function Permissions() {
             <h2>Permissions</h2>
             
             <table className={styles.permissionsTable}>
-                <tr><th></th>{tableHeaders.map(header => <th>{header}</th>)}</tr>
-                
-                {tableContents.map(({name, permissions}) => 
-                    <tr>
-                        <td>X</td>
-                        <td><input type="text" defaultValue={name}></input></td>
-                        {permissions.map(allowed => <td><input type="checkbox" defaultChecked={allowed}></input></td>)}
-                    
-                    </tr>
-                )}
+                <thead>
+                    <tr>{tableHeaders.map(header => <th>{header}</th>)}</tr>
+                </thead>
+                <tbody>
+                    {tableContents.map(({name, roleId, permissions}) => 
+                        <tr>
+                            <td><TextInput label="" defaultValue={name}/></td>
+                            {permissions.map(allowed => <td><input type="checkbox" defaultChecked={allowed}></input></td>)}
+                            <td><DeleteRoleButton roleId={roleId} /></td>
+                        </tr>
+                    )}
+                </tbody>
             </table>
             </div>
 
