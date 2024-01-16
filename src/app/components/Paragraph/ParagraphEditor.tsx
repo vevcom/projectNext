3fabfef566418b87e269aeab795b2aa20c274cf9
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-16 18:36:09 +0100

    fix: more styling

--- src/app/components/Paragraph/ParagraphEditor.tsx
+++ src/app/components/Paragraph/ParagraphEditor.tsx
@@ -2,13 +2,14 @@
 import { EditModeContext } from '@/context/EditMode';
 import styles from './ParagraphEditor.module.scss';
 import { useContext, useState } from 'react';
-import type { Paragraph } from '@prisma/client';
-import Form from '@/components/Form/Form';
-import update from '@/actions/paragraphs/update';
-import { useRouter } from 'next/navigation';
-import SimpleMDEEditor from 'react-simplemde-editor';
+import type { Paragraph } from '@prisma/client'
+import Form from '@/components/Form/Form'
+import update from '@/actions/paragraphs/update'
+import { useRouter } from 'next/navigation'
+import SimpleMDEEditor from 'react-simplemde-editor'
+import 'easymde/dist/easymde.min.css'
 import './CustomEditorClasses.scss'
-import 'easymde/dist/easymde.min.css';
+
 
 type PropTypes = {
     paragraph: Paragraph
@@ -28,7 +29,7 @@ const ParagraphEditor = ({ paragraph }: PropTypes) => {
     return (
         editmode.editMode && (
             <div className={styles.ParagraphEditor}>
-                <SimpleMDEEditor value={content} onChange={handleContentChange} />
+                <SimpleMDEEditor className={styles.editor} value={content} onChange={handleContentChange} />
                 <Form
                     action={update.bind(null, paragraph.id).bind(null, content)}
                     submitText='Update'
