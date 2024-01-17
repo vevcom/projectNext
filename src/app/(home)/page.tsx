82e3bee14fe7d530faa8b15ec7296b712f677997
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-17 15:51:48 +0100

    seeded images was in wrong directory

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
                         </Link>
                     </div>
                 </div>
-                <Section imagename="kappemann" name="Om Omega" lesMer="infopages/about" imgWidth={450} id="firstSection">
+                <Section imagename="frontpage_1" name="Om Omega" lesMer="infopages/about" imgWidth={450} id="firstSection">
           Sct. Omega Broderskab ble offisielt stiftet 21. november 1919 av et kull elektrostudenter
           som må ha hatt et svært godt kameratskap og sosialt miljø. Til å begynne med var det en
           eksklusiv klubb forbeholdt 3. og 4. årskurs, og for å bli tatt opp måtte man sende inn
