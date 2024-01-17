b9bf7acd4adec12bc12ca617f9635f1c93a55780
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-17 12:52:43 +0100

    fix: must call seedCms in seed

--- src/prisma/seeder/seed.ts
+++ src/prisma/seeder/seed.ts
@@ -1,10 +1,12 @@
 import seedImages from "./seedImages";
 import seedDevUsers from "./development/seedDevUsers";
 import seedDevImages from "./development/seedDevImages";
+import seedCms from "./seedCms";
 
 export default async function seed() {
     console.log('seeding standard data....')
     await seedImages()
+    await seedCms()
     console.log('seed standard done')
 
     if (process.env.NODE_ENV !== 'development') return
