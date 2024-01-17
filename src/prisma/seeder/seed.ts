7ecb5f8db23f6e57e6c016fe4d775f9e3dc16b08
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-17 12:33:30 +0100

    chore: creates new seed file

--- src/prisma/seeder/seed.ts
+++ src/prisma/seeder/seed.ts
@@ -1,12 +1,15 @@
 import seedImages from "./seedImages";
+import seedDevUsers from "./development/seedDevUsers";
+import seedDevImages from "./development/seedDevImages";
 
 export default async function seed() {
     console.log('seeding standard data....')
     await seedImages()
-
     console.log('seed standard done')
+
     if (process.env.NODE_ENV !== 'development') return
     console.log('seeding dev data....')
-
+    await seedDevUsers();
+    await seedDevImages();
     console.log('seed dev done')
 }
