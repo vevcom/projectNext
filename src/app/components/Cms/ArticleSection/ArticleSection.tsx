77daca96f369bec397507c909451421ef5c8f281
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-23 17:27:08 +0100

    chore: adds a lot of boilerplate to start the great work

---
+++ src/app/components/Cms/ArticleSection/ArticleSection.tsx
@@ -0,0 +1,9 @@
+import styles from './ArticleSection.module.scss'
+
+export default function ArticleSection() {
+    return (
+        <span className={styles.ArticleSection}>
+            <h1>ArticleSection</h1>
+        </span>
+    );
+}
---
+++ src/app/components/Cms/CmsLink/CmsLink.tsx
@@ -0,0 +1,9 @@
+import styles from './CmsLink.module.scss'
+
+export default function CmsLink() { 
+    return (
+        <div className={styles.cmsLink}>
+            <h1>CmsLink</h1>
+        </div>
+    );
+}
