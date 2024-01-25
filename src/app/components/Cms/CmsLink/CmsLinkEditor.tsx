76656eb231a8de8f3b776faf3f4ddfa4e0acfb42
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-01-25 19:36:48 +0100

    chore: cmsLinkEditor should not show in nonedit mode

--- src/app/components/Cms/CmsLink/CmsLinkEditor.tsx
+++ src/app/components/Cms/CmsLink/CmsLinkEditor.tsx
@@ -1,14 +1,20 @@
+'use client'
 import styles from './CmsLinkEditor.module.scss'
 import Form from '@/components/Form/Form';
 import TextInput from '../../UI/TextInput';
 import type { CmsLink } from '@prisma/client';
 import update from '@/actions/cms/links/update';
+import { EditModeContext } from '@/context/EditMode';
+import { useContext } from 'react';
 
 type PropTypes = {
     cmsLink: CmsLink
 }
 
 export default function CmsLinkEditor({ cmsLink }: PropTypes) {
+    const editContext = useContext(EditModeContext)
+    if (!editContext?.editMode) return null
+
     return (
         <Form 
             className={styles.CmsLinkEditor}
