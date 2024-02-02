5837197ffb2a8f13b9de196d436700f2c67122b1
Paulius Juzenas <59730296+Paulijuz@users.noreply.github.com>
2024-02-02 01:34:50 +0100

    chore: add support for new action return type

--- src/app/admin/permissions/page.tsx
+++ src/app/admin/permissions/page.tsx
@@ -1,57 +1,59 @@
 import styles from './page.module.scss'
 import CreateRoleForm from './CreateRoleForm'
 
-import { readRoles } from '@/actions/permissions'
 import DeleteRoleButton from './DeleteRoleButton'
+import { readRoles } from '@/actions/permissions'
 import TextInput from '@/app/components/UI/TextInput'
 import { Permission } from '@prisma/client'
 
 export default async function Permissions() {
-    const {data: roles = []} = await readRoles()
+    const res = await readRoles()
+
+    if (!res.success) throw Error(res.error ? res.error[0].message : 'error')
+
+    const { data: roles } = res
 
     const permissionNames: Record<Permission, string> = {
-        USE_MONEY: "Bruke penger",
-        USE_FRIDGE: "Bruke kioleskab",
-        INFINITE_MONEY: "Evig peneger",
-        PARTICIPATE_IN_EVENTS: "Delta på hendelser",
-        CREATE_EVENTS: "Lage hendelser",
-        USE_OMEGA_QUOTES: "Skrive Omega Quotes",
-        POST_BULSHIT: "Skrive bulshit",
-        VIEW_BULSHIT: "Lese bulshit",
-        CREATIVE_MODE: "Kreativ-modus",
+        USE_MONEY: 'Bruke penger',
+        USE_FRIDGE: 'Bruke kioleskab',
+        INFINITE_MONEY: 'Evig peneger',
+        PARTICIPATE_IN_EVENTS: 'Delta på hendelser',
+        CREATE_EVENTS: 'Lage hendelser',
+        USE_OMEGA_QUOTES: 'Skrive Omega Quotes',
+        POST_BULSHIT: 'Skrive bulshit',
+        VIEW_BULSHIT: 'Lese bulshit',
+        CREATIVE_MODE: 'Kreativ-modus',
     }
 
-    const tableContents = roles.map(role => { 
-        return {
-            name: role.name, 
-            roleId: role.id,
-            permissions: role.permissions.map(permission => permission.permission)
-        }
-    })
+    const tableContents = roles.map(role => ({
+        name: role.name,
+        roleId: role.id,
+        permissions: role.permissions.map(permission => permission.permission)
+    }))
 
     return (
         <div className={styles.permissionsWrapper}>
             <div className={styles.permissionsDiv}>
-            <h2>Permissions</h2>
-            
-            <table className={styles.permissionsTable}>
-                <thead>
-                    <tr><th>Navn</th>{Object.values(permissionNames).map(header => <th>{header}</th>)}</tr>
-                </thead>
-                <tbody>
-                    {tableContents.map(({name, roleId, permissions}) => 
-                        <tr>
-                            <td><TextInput label="" defaultValue={name}/></td>
+                <h2>Permissions</h2>
 
-                            {Object.keys(permissionNames).map(permission => 
-                                <td><input type="checkbox" defaultChecked={permissions.includes(permission as Permission)} /></td>
-                            )}
-                            
-                            <td><DeleteRoleButton roleId={roleId} /></td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
+                <table className={styles.permissionsTable}>
+                    <thead>
+                        <tr><th>Navn</th>{Object.values(permissionNames).map(header => <th>{header}</th>)}</tr>
+                    </thead>
+                    <tbody>
+                        {tableContents.map(({ name, roleId, permissions }) =>
+                            <tr>
+                                <td><TextInput label="" defaultValue={name}/></td>
+
+                                {Object.keys(permissionNames).map(permission =>
+                                    <td><input type="checkbox" defaultChecked={permissions.includes(permission as Permission)} /></td>
+                                )}
+
+                                <td><DeleteRoleButton roleId={roleId} /></td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
             </div>
 
             <div>
