8769346c20c45bf0ae08aecf9723804c634ae019
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2024-02-03 15:32:36 +0100

    Add: omegaquote popup

--- src/app/omegaquotes/page.tsx
+++ src/app/omegaquotes/page.tsx
@@ -1,13 +1,18 @@
-import Button from '../components/UI/Button'
 import styles from './page.module.scss'
 import PageWrapper from '@/components/PageWrapper/pageWrapper'
+import PopUp from '@/components/PopUp/PopUp'
+import Form from '@/components/Form/Form'
+import create from '@/actions/quotes/create'
+import TextInput from '@/components/UI/TextInput'
 
 export default async function OmegaQuotes() {
     return (
         <PageWrapper title="Omega Qutoes" headerItem={
-            <Button className={styles.button}>
-                Ny omegaquote
-            </Button> 
+            <PopUp PopUpKey="new_omega_quote" showButtonContent="Ny Omegaquote" showButtonClass={styles.button}>
+                <Form title="Ny Omegaquote" submitText="Legg til" action={create}>
+                    <TextInput label="Sagt av" name="said_by" />
+                </Form>
+            </PopUp>
         }>
             <div className={styles.OmegaQuotes}>
                 Content
