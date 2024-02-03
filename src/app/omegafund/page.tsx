e02c4555e9b67da0916d8fb773efeb0b776b97c2
Gaute <gaute.flagstad@gmail.com>
2024-02-03 15:10:23 +0100

    feat: omegafund page

---
+++ src/app/omegafund/page.tsx
@@ -0,0 +1,14 @@
+import styles from "./page.module.scss"
+
+export default function OmegaFund(){
+    return <div className= {styles.wrapper}>
+        <h1>Omegafond</h1>
+            <iframe 
+                src="https://www.shareville.no/widget/portfolio/590617/yield?period=180"
+                allowFullScreen
+                className={styles.omegafundIframe}
+            >  
+            </iframe>
+        
+    </div>
+}
