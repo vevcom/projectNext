a81b5c3f1af53895d900bbfe816304669bf256bc
Gaute <gaute.flagstad@gmail.com>
2024-02-03 15:17:29 +0100

    fix: fixed header

--- src/app/omegafund/page.tsx
+++ src/app/omegafund/page.tsx
@@ -2,7 +2,7 @@ import styles from "./page.module.scss"
 
 export default function OmegaFund(){
     return <div className= {styles.wrapper}>
-        <h1>Omegafond</h1>
+        <h1>Omegafondet</h1>
             <iframe 
                 src="https://www.shareville.no/widget/portfolio/590617/yield?period=180"
                 allowFullScreen
