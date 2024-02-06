ba83fdca27584f2b3fe35cdf7b2189a0d3d74336
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-06 14:15:12 +0100

    feat: adds action to move a section

--- src/actions/cms/articles/update.ts
+++ src/actions/cms/articles/update.ts
@@ -6,6 +6,7 @@ import type { ReturnType } from './ReturnType'
 import { maxSections } from './ConfigVars'
 import { addPart } from '@/cms/articleSections/update'
 import type { Part } from '@/cms/articleSections/update'
+import { ArticleSection } from '@prisma/client'
 
 export default async function update(id: number, config: {
     name?: string,
@@ -35,7 +36,10 @@ export default async function update(id: number, config: {
     }
 }
 
-export async function addSectionToArticle(id: number, include: Partial<Record<Part, boolean>>) : Promise<ActionReturn<ReturnType>> {
+export async function addSectionToArticle(
+    id: number, 
+    include: Partial<Record<Part, boolean>>
+) : Promise<ActionReturn<ReturnType>> {
     try {
         const article = await prisma.article.findUnique({
             where: {
@@ -113,6 +117,78 @@ export async function addSectionToArticle(id: number, include: Partial<Record<Pa
         }
 
         return { success: true, data: updatedArticle }
+    } catch (error) {
+        return errorHandler(error)
+    }
+}
+
+export async function moveSectionOrder(
+    sectionId: number, 
+    direction: 'UP' | 'DOWN'
+) : Promise<ActionReturn<ArticleSection>> {
+    try {
+        //get section to move
+        const section = await prisma.articleSection.findUnique({
+            where: {
+                id: sectionId,
+            },
+        })
+        if (!section) return {
+            success: false,
+            error: [{
+                message: 'Seksjon ikke funnet',
+            }],
+        }
+
+        //find the section with the order one higher/lower than the current section
+        const otherSection = await prisma.articleSection.findFirst({
+            where: {
+                order: direction === 'UP' ? {
+                    lt: section.order,
+                } : {
+                    gt: section.order,
+                },
+            },
+            orderBy: {
+                order: direction === 'UP' ? 'desc' : 'asc',
+            },
+        })
+        if (!otherSection) return {
+            success: false,
+            error: [{
+                message: 'Seksjon kan ikke  flyttes opp/ned',
+            }],
+        }
+
+
+        //flip thir order numbers
+        const [updatedSection, updatedOtherSection] = await prisma.$transaction([
+            prisma.articleSection.update({
+                where: {
+                    id: section.id,
+                },
+                data: {
+                    order: otherSection.order,
+                },
+            }),
+            prisma.articleSection.update({
+                where: {
+                    id: otherSection.id,
+                },
+                data: {
+                    order: section.order,
+                },
+            }),
+        ])
+
+        if (!updatedSection || !updatedOtherSection) return {
+            success: false,
+            error: [{
+                message: 'Noe uventet skjedde under flytting av seksjonen',
+            }],
+        }
+
+        return { success: true, data: updatedSection }
     } catch (error) {
         return errorHandler(error)
     }
