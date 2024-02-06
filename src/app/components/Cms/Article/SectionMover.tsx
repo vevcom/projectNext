64d59e70a3c381efbbee7907442e84d6216f7cc6
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-06 13:40:58 +0100

    chore: adds styling to move section buttons

--- src/app/components/Cms/Article/SectionMover.tsx
+++ src/app/components/Cms/Article/SectionMover.tsx
@@ -4,13 +4,14 @@ import { EditModeContext } from "@/context/EditMode"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons"
 import { useContext } from "react"
+import styles from './SectionMover.module.scss'
 
 export default function SectionMover() {
     const editMode = useContext(EditModeContext)
     if (!editMode?.editMode) return null
 
     return (
-        <div>
+        <div className={styles.SectionMover}>
             <button>
                 <FontAwesomeIcon icon={faArrowUp} />
             </button>
