038d593bbd3acbd82e65a6f44149485d23f93deb
William Juul Stensrud <williamjuuls@gmail.com>
2024-02-06 18:12:18 +0100

    feat: add user read action

---
+++ src/actions/users/read.ts
@@ -0,0 +1,26 @@
+"use server"
+import prisma from '@/prisma'
+import { ActionReturn } from "../type";
+import type { User } from '@prisma/client'
+import errorHandler from '@/prisma/errorHandler';
+
+export default async function read(id: number) : Promise<ActionReturn<User>> {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id,
+            },
+        })
+
+        if (!user) {
+            return {success: false, error: [{message: "User not found"}]}
+        }
+        else {
+            return { success: true, data: user}
+        }
+    }
+    catch(error) {
+        return errorHandler(error)
+    }
+}
+
