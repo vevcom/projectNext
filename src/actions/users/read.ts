715835722409ed409e0848c6406bc90c3a86da17
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-06 20:01:00 +0100

    fix: missing quote

--- src/actions/users/read.ts
+++ src/actions/users/read.ts
@@ -3,7 +3,7 @@ import prisma from '@/prisma'
 import errorHandler from '@/prisma/errorHandler'
 import type { Image } from '@prisma/client'
 import type { ActionReturn, ReadPageInput } from '@/actions/type'
-import { ReturnType } from './ReturnType
+import { ReturnType } from './ReturnType'
 
 export default async function readUserPage<const PageSize extends number>({ 
     page, 
