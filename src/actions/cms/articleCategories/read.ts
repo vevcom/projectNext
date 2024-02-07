2680dbda8f647da8301416bb5011abc620c9d82d
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-07 09:39:04 +0100

    feat: read many categories action

---
+++ src/actions/cms/articleCategories/read.ts
@@ -0,0 +1,15 @@
+'use server'
+
+import prisma from "@/prisma"
+import type { ActionReturn } from "@/actions/type"
+import type { ArticleCategory } from '@prisma/client'
+import errorHandler from '@/prisma/errorHandler'
+
+export default async function readArticleCategories(): Promise<ActionReturn<ArticleCategory[]>> {
+    try {
+        const categories = await prisma.articleCategory.findMany()
+        return { success: true, data: categories }
+    } catch (error) {
+        return errorHandler(error)
+    }
+}
