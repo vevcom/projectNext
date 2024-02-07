93fa84d63bdf3e1cb60cb13b0a505839134fa8ef
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-07 19:14:20 +0100

    chore: fixes some general styling things

--- src/app/articles/[category]/layout.tsx
+++ src/app/articles/[category]/layout.tsx
@@ -20,6 +20,7 @@ export default async function ArticleCategoryLayout({ params, children }: PropTy
             <aside>
                 <SideBar category={category} />
             </aside>
+            
             <main>
                 {children}
             </main>
