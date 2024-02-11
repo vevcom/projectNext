7f96bf03fd464e6bbdc07202a08685a487c68c7e
Paulius <pauliusj@stud.ntnu.no>
2024-02-11 18:54:11 +0100

    chore: remove log directory env variable

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,12 +1,14 @@
 import winston from 'winston'
 import 'winston-daily-rotate-file'
 
+console.log(`Logger created with parameter ${process.env.LOG_LEVEL}, ${process.env.LOG_MAX_FILES}`)
+
 const logger = winston.createLogger({
     level: process.env.LOG_LEVEL,
     transports: [
         new winston.transports.Console(),
         new winston.transports.DailyRotateFile({
-            dirname: process.env.LOG_DIRECTORY,
+            dirname: './logs',
             filename: '%DATE%.log',
             datePattern: 'YYYY-MM-DD',
             maxFiles: process.env.LOG_MAX_FILES,
