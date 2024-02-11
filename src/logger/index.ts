06b8b56a4edb6dcc0e616df78c2fff410aa19ce1
Paulius <pauliusj@stud.ntnu.no>
2024-02-11 18:07:29 +0100

    feat: add logger

---
+++ src/logger/index.ts
@@ -0,0 +1,17 @@
+import winston from 'winston'
+import 'winston-daily-rotate-file'
+
+const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL,
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.DailyRotateFile({
+            dirname: process.env.LOG_DIRECTORY,
+            filename: "%DATE%.log",
+            datePattern: "YYYY-MM-DD",
+            maxFiles: process.env.LOG_MAX_FILES,
+        })
+    ]
+}) 
+
+export default logger
