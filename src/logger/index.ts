cfe01bd1e3237733211fc64d6d4fb8bc6f32ad5b
Paulius <pauliusj@stud.ntnu.no>
2024-02-11 19:21:37 +0100

    feat: choosable log destination

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,19 +1,21 @@
 import winston from 'winston'
 import 'winston-daily-rotate-file'
 
-console.log(`Logger created with parameter ${process.env.LOG_LEVEL}, ${process.env.LOG_MAX_FILES}`)
-
 const logger = winston.createLogger({
     level: process.env.LOG_LEVEL,
-    transports: [
-        new winston.transports.Console(),
-        new winston.transports.DailyRotateFile({
-            dirname: './logs',
-            filename: '%DATE%.log',
-            datePattern: 'YYYY-MM-DD',
-            maxFiles: process.env.LOG_MAX_FILES,
-        })
-    ]
 })
+
+if(process.env.LOG_TO_CONSOLE === 'true') {
+    logger.add(new winston.transports.Console())
+}
+
+if(process.env.LOG_TO_FILE === 'true') {
+    logger.add(new winston.transports.DailyRotateFile({
+        dirname: './logs',
+        filename: '%DATE%.log',
+        datePattern: 'YYYY-MM-DD',
+        maxFiles: process.env.LOG_MAX_FILES,
+    }))
+}
 
 export default logger
