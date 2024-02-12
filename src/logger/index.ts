7963e07ae640bd9b4bf201b2aa03f726dff309ca
Paulius Juzenas <59730296+Paulijuz@users.noreply.github.com>
2024-02-12 23:54:37 +0100

    fix: linting

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -5,11 +5,11 @@ const logger = winston.createLogger({
     level: process.env.LOG_LEVEL,
 })
 
-if(process.env.LOG_TO_CONSOLE === 'true') {
+if (process.env.LOG_TO_CONSOLE === 'true') {
     logger.add(new winston.transports.Console())
 }
 
-if(process.env.LOG_TO_FILE === 'true') {
+if (process.env.LOG_TO_FILE === 'true') {
     logger.add(new winston.transports.DailyRotateFile({
         dirname: './logs',
         filename: '%DATE%.log',
