4dba8cbcca54ef5cbfbfe910876c13062d0e37e0
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-12 18:45:25 +0100

    chore: adds boilerplate for ImageCard component

---
+++ src/app/components/ImageCard/ImageCard.tsx
@@ -0,0 +1,9 @@
+import styles from './ImageCard.module.css';
+
+export default function ImageCard() {
+    return (
+        <div className={styles.ImageCard}>
+            ImageCard
+        </div>
+    )
+}
