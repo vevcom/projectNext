f17cfce34de7ed4dc653c579bfb4436214d83b87
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-12 21:05:46 +0100

    chore: adds some boilerplate to later add visibility to articleCategories

--- src/actions/cms/articleCategories/update.ts
+++ src/actions/cms/articleCategories/update.ts
@@ -1,1 +1,10 @@
 'use server'
+import type { ReturnType } from "./ReturnType"
+import { ActionReturn } from "@/actions/type"
+
+export default function updateArticleVisibility(
+    id: number, 
+    visibility: unknown
+): Promise<ActionReturn<ReturnType>> {
+    throw new Error('Not implemented')
+}
