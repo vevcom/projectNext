914a9d20a7e5aaaf05ecfb878e83d7023fb40e6c
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-12 18:51:09 +0100

    chore: adds functionality for passing image to ImageCard component

--- src/app/components/ImageCard/ImageCard.tsx
+++ src/app/components/ImageCard/ImageCard.tsx
@@ -1,9 +1,16 @@
-import styles from './ImageCard.module.css';
+import { Image as ImageT } from '@prisma/client';
+import styles from './ImageCard.module.scss';
+import Image from '@/components/Image/Image';
 
-export default function ImageCard() {
+type PropTypes = {
+    image: ImageT;
+}
+
+export default function ImageCard({ image }: PropTypes) {
     return (
         <div className={styles.ImageCard}>
             ImageCard
+            <Image width={200} image={image} />
         </div>
     )
 }
--- src/app/jobads/page.tsx
+++ src/app/jobads/page.tsx
@@ -1,11 +1,17 @@
 import styles from './page.module.scss';
 import PageWrapper from '../components/PageWrapper/PageWrapper';
+import ImageCard from '../components/ImageCard/ImageCard';
+import { readImage } from '@/actions/images/read';
 
-export default function JobAds() {
+export default async function JobAds() {
+    const testImageRes = await readImage('ohma');
+    if (!testImageRes.success) throw new Error('Failed to read image');
+    const testImage = testImageRes.data;
+
     return (
         <PageWrapper title="Job ads">
             <div className={styles.wrapper}>
-                job ad
+                <ImageCard image={testImage} />
             </div>
         </PageWrapper>
         
