df0506fcaad34e5675a91786d43789b80e770167
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-13 16:46:45 +0100

    chore: adds styling and call to update action

--- src/app/articles/[category]/EditCategory.tsx
+++ src/app/articles/[category]/EditCategory.tsx
@@ -5,8 +5,14 @@ import { faBolt } from '@fortawesome/free-solid-svg-icons'
 import PopUp from '@/app/components/PopUp/PopUp'
 import Textarea from '@/app/components/UI/Textarea'
 import TextInput from '@/app/components/UI/TextInput'
+import { ReturnType } from '@/actions/cms/articleCategories/ReturnType'
+import { updateArticleCategory } from '@/actions/cms/articleCategories/update'
 
-export default function EditCategory() {
+type PropTypes = {
+    category: ReturnType
+}
+
+export default function EditCategory({ category }: PropTypes) {
     //TODO: check if user has edit visibility
     const canEditArticle = true
 
@@ -20,10 +26,15 @@ export default function EditCategory() {
             >
                 <Form
                     className={styles.EditCategory}
-                    action={update}
+                    action={updateArticleCategory.bind(null, category.id)}
                 >
                     <TextInput label="navn" name="name" defaultValue={"hei"} />
-                    <Textarea label="beskrivelse" name="description" defaultValue={"hei"} />
+                    <Textarea 
+                        label="beskrivelse" 
+                        name="description" 
+                        defaultValue={"hei"} 
+                        className={styles.description}
+                    />
                 </Form>
             </PopUp>
         )
