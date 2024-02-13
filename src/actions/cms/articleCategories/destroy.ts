c9ea36ce76f7280b92cd6125fe6db988062a0648
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-13 12:24:16 +0100

    feat: adds destroy action for category

--- src/actions/cms/articleCategories/destroy.ts
+++ src/actions/cms/articleCategories/destroy.ts
@@ -1,1 +1,23 @@
 'use server'
+
+import prisma from '@/prisma'
+import errorHandler from '@/prisma/errorHandler'
+import type { ReturnType } from './ReturnType'
+import type { ActionReturn } from '@/actions/type'
+
+export default async function destroyArticleCategory(id: number): Promise<ActionReturn<ReturnType>> {
+    try {
+        // Cheek for visibility type edit of user.
+        const articleCategory = await prisma.articleCategory.delete({
+            where: {
+                id
+            },
+            include: {
+                articles: true
+            }
+        })
+        return { success: true, data: articleCategory }
+    } catch (error) {
+        return errorHandler(error)
+    }
+}
