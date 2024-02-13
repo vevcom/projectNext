896e5e66f62bd8f119a15973e5d583390e51a16a
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-13 16:42:25 +0100

    feat: create update action for articlecat.

--- src/actions/cms/articleCategories/update.ts
+++ src/actions/cms/articleCategories/update.ts
@@ -2,14 +2,43 @@
 import type { ReturnType } from "./ReturnType"
 import { ActionReturn } from "@/actions/type"
 import schema from "./schema"
+import prisma from "@/prisma"
+import errorHandler from "@/prisma/errorHandler"
 
-export default async function updateArticleCategoryVisibility(
+export async function updateArticleCategoryVisibility(
     id: number, 
     visibility: unknown
 ): Promise<ActionReturn<ReturnType>> {
     throw new Error('Not implemented')
 }
 
-export default async function updateArticleCategory(
-
-)
+export async function updateArticleCategory(
+    id: number,
+    rawData: FormData
+) : Promise<ActionReturn<ReturnType>> {
+    const parse = schema.safeParse({
+        name: rawData.get('name'),
+        description: rawData.get('description'),
+    })
+    if (!parse.success) {
+        return { success: false, error: parse.error.issues }
+    }
+    const { name, description } = parse.data
+    try {
+        const articleCategory = await prisma.articleCategory.update({
+            where: {
+                id
+            },
+            data: {
+                name,
+                description
+            },
+            include: {
+                articles: true
+            }
+        })
+        return { success: true, data: articleCategory }
+    } catch (error) {
+        return errorHandler(error)
+    }
+}
