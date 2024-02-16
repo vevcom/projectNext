1f349a9ae586b305a12fee0adb38434ac96ebae6
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-16 18:56:36 +0100

    fix: imported type had moved location

--- src/context/paging/omegaquotesPaging.tsx
+++ src/context/paging/omegaquotesPaging.tsx
@@ -2,7 +2,7 @@
 import generatePagingProvider, { generatePagingContext } from '@/context/paging/PagingGenerator'
 import { readQuotesPage } from '@/actions/omegaquotes/read'
 import type { ReadPageInput } from '@/actions/type'
-import type { OmegaquoteFiltered } from '@/actions/omegaquotes/read'
+import type { OmegaquoteFiltered } from '@/actions/omegaquotes/Types'
 
 export type PageSizeOmegaquote = 20;
 const fetcher = async (x: ReadPageInput<PageSizeOmegaquote>) => await readQuotesPage(x)
