538fb20ef96236b9c89ed08593a6353f7fcfd436
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-20 11:54:44 +0100

    chore: adds DateInput component

---
+++ src/app/components/UI/DateInput.tsx
@@ -0,0 +1,9 @@
+import styles from './DateInput.module.scss'
+
+export default function DateInput() {
+    return (
+        <div className={styles.DateInput}>
+            <input type="date" />
+        </div>
+    )
+}
