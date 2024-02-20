9c60ce0e7112699d18cb3771ed5a41d25f9727cb
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-20 18:31:01 +0100

    chore: adds Endless scroll

--- src/app/news/archive/OldNewsList.tsx
+++ src/app/news/archive/OldNewsList.tsx
@@ -1,6 +1,9 @@
 'use client'
 import { ReactNode } from 'react'
 import styles from './OldNewsList.module.scss'
+import EndlessScroll from '@/app/components/PagingWrappers/EndlessScroll'
+import { OldNewsPagingContext } from '@/context/paging/OldNewsPaging'
+import NewsCard from '@/app/news/NewsCard'
 
 type PropTypes = {
     serverRendered: ReactNode
@@ -10,6 +13,11 @@ export default function OldNewsList({ serverRendered }: PropTypes) {
     return (
         <div className={styles.OldNewsList}>
             { serverRendered }
+            <EndlessScroll 
+                pagingContext={OldNewsPagingContext} 
+                renderer={news => (
+                    <NewsCard news={news} />
+                )} />
         </div>
     )
 }
