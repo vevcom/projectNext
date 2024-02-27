880d8810e7c30c4ece0245f9022bc23117367adf
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-27 18:32:22 +0100

    chore: read action require permission

--- src/actions/ombul/read.ts
+++ src/actions/ombul/read.ts
@@ -4,8 +4,21 @@ import { ActionReturn } from "@/actions/Types";
 import prisma from "@/prisma";
 import errorHandler from "@/prisma/errorHandler";
 import { ExpandedOmbul } from "./Types";
+import { getUser } from "@/auth";
 
 export async function readLatestOmbul() : Promise<ActionReturn<Ombul>> {
+    //Auth route
+    const { user, status } = await getUser({
+        permissions: ['OMBUL_READ']
+    })
+    if (!user) {
+        return {
+            success: false,
+            error: [{
+                message: status
+            }]
+        }
+    }
     try {
         const ombul = await prisma.ombul.findMany({
             orderBy: [
@@ -33,6 +46,19 @@ export async function readLatestOmbul() : Promise<ActionReturn<Ombul>> {
 }
 
 export async function readOmbuls() : Promise<ActionReturn<ExpandedOmbul[]>> {
+    //Auth route
+    const { user, status } = await getUser({
+        permissions: ['OMBUL_READ']
+    })
+    if (!user) {
+        return {
+            success: false,
+            error: [{
+                message: status
+            }]
+        }
+    }
+
     try {
         const ombuls = await prisma.ombul.findMany({
             orderBy: [
