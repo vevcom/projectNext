968412822d4ecc98c54157272f0242d10b30b16a
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-02-27 15:34:16 +0100

    chore: adds CreateOmbul styling

--- src/app/ombul/CreateOmbul.tsx
+++ src/app/ombul/CreateOmbul.tsx
@@ -5,6 +5,7 @@ import Form from '@/components/Form/Form'
 import styles from './CreateOmbul.module.scss'
 import TextInput from '../components/UI/TextInput'
 import NumberInput from '../components/UI/NumberInput'
+import FileInput from '../components/UI/FileInput'
 
 /**
  * This component is for creating ombul issues. Since it needs to be able to choose a image
@@ -18,9 +19,14 @@ export default function CreateOmbul() {
         <div className={styles.CreateOmbul}>
             <Form
                 action={createOmbul}
+                submitText="Lag ombul"
+                className={styles.form}
             >
                 <TextInput label="navn" name="name" />
                 <NumberInput label="År" name="year" defaultValue={currentYear} />
+                <NumberInput label="nummer" name="issueNumber" />
+                <FileInput color="primary" label="Ombul fil" name="ombulFile" />
+                <FileInput color="primary" label="Ombul cover" name="ombulCoverImage" />
             </Form>
             <div className={styles.chooseImage}>
 
