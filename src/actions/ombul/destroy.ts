2ec01ccc816b71c6265153228138937810e27a12
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-04 21:56:32 +0100

    feat: delete ombul action

--- src/actions/ombul/destroy.ts
+++ src/actions/ombul/destroy.ts
@@ -1,1 +1,47 @@
 'use server'
+import type { ExpandedOmbul } from "./Types"
+import type { ActionReturn } from "@/actions/Types"
+import prisma from "@/prisma"
+import errorHandler from "@/prisma/errorHandler"
+import deleteFile from "@/store/deleteFile"
+
+export async function destroyOmbul(id: number): Promise<ActionReturn<ExpandedOmbul>> {
+    try {
+        const ombul = await prisma.ombul.findUnique({
+            where: {
+                id
+            },
+            include: {
+                coverImage: {
+                    include: {
+                        image: true
+                    }
+                }
+            }
+        })
+        if (!ombul) {
+            return {
+                success: false,
+                error: [{
+                    message: 'Ombul ikke funnet'
+                }]
+            }
+        }
+
+        const res = await deleteFile('ombul', ombul.fsLocation)
+        if (!res.success) return res
+
+        await prisma.ombul.delete({
+            where: {
+                id
+            }
+        })
+
+        return {
+            success: true,
+            data: ombul
+        }
+    } catch (error) {
+        return errorHandler(error)
+    }
+}
