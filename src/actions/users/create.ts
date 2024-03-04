11f159a1d32014d46bb9645441798d406a39da8e
Paulius <pauliusj@stud.ntnu.no>
2024-03-04 15:29:44 +0100

    feat: formdata and object intercompability

--- src/actions/users/create.ts
+++ src/actions/users/create.ts
@@ -2,26 +2,23 @@
 import prisma from '@/prisma'
 import errorHandler from '@/prisma/errorHandler'
 import { z } from 'zod'
+import { zfd } from 'zod-form-data'
 import type { ActionReturn } from '@/actions/Types'
 import type { User } from '@prisma/client'
 
-export async function createUser(rawdata: FormData): Promise<ActionReturn<User>> {
-    const schema = z.object({
-        username: z.string().max(50).min(2),
-        password: z.string().max(50).min(2),
-        email: z.string().max(200).min(2).email(),
-        firstname: z.string().max(50).min(2),
-        lastname: z.string().max(50).min(2),
-        confirmPassword: z.string().max(50).min(2),
-    }).refine((data) => data.password === data.confirmPassword, 'Password must match confirm password')
-    const parse = schema.safeParse({
-        username: rawdata.get('username'),
-        password: rawdata.get('password'),
-        email: rawdata.get('email'),
-        firstname: rawdata.get('firstname'),
-        lastname: rawdata.get('lastname'),
-        confirmPassword: rawdata.get('confirmPassword'),
-    })
+const userSchema = zfd.formData({
+    username: z.string().max(50).min(2),
+    password: z.string().max(50).min(2),
+    email: z.string().max(200).min(2).email(),
+    firstname: z.string().max(50).min(2),
+    lastname: z.string().max(50).min(2),
+    confirmPassword: z.string().max(50).min(2),
+}).refine((data) => data.password === data.confirmPassword, 'Password must match confirm password')
+
+type createUserArgs = z.infer<typeof userSchema>
+
+export async function createUser(rawdata: FormData | createUserArgs): Promise<ActionReturn<User>> {
+    const parse = userSchema.safeParse(rawdata)
 
     if (!parse.success) {
         return { success: false, error: parse.error.issues }
