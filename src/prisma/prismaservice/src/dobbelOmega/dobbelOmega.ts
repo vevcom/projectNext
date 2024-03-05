2c72f150677036a66d9dd030f252fae563037700
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-05 23:00:20 +0100

    fix: VEVEN_DB_URI was in wrong container

--- src/prisma/prismaservice/src/dobbelOmega/dobbelOmega.ts
+++ src/prisma/prismaservice/src/dobbelOmega/dobbelOmega.ts
@@ -1,6 +1,8 @@
 import { PrismaClient as PrismaClientVeven } from '@/generated/veven'
 import type { PrismaClient as PrismaClientPn } from '@/generated/pn'
+import migrateOmbul from './migrateOmbul'
 
 export default async function dobbelOmega(pnPrisma: PrismaClientPn) {
     const vevenPrisma = new PrismaClientVeven()
+    migrateOmbul(pnPrisma, vevenPrisma)
 }
--- src/prisma/prismaservice/src/dobbelOmega/migrateOmbul.ts
+++ src/prisma/prismaservice/src/dobbelOmega/migrateOmbul.ts
@@ -4,7 +4,29 @@ import type { PrismaClient as PrismaClientVeven } from '@/generated/veven'
 export default async function migrateOmbul(pnPrisma: PrismaClientPn, vevenPrisma: PrismaClientVeven) {
     const ombuls = await vevenPrisma.ombul.findMany()
     Promise.all(ombuls.map(async (ombul) => {
-        const fsLocation = ''
+        const fsLocationOldVev = `${process.env.VEVEN_STORE_URL}/ombul/${ombul.fileName}/${ombul.originalName}`
+
+        // Get pdf served at old location
+        const res = await fetch(fsLocationOldVev, {
+            method: 'GET',
+        })
+        const pdf = await res.blob()
+        console.log(res)
+
+        const fsLocation = `hei`
+
+        const coverName = ombul.title.split(' ').join('_') + '_cover'
+        const coverImage = await pnPrisma.cmsImage.upsert({
+            where: {
+                name: coverName
+            },
+            update: {
+
+            },
+            create: {
+                name: coverName,
+            }
+        })
 
         return pnPrisma.ombul.upsert({
             where: {
@@ -14,14 +36,18 @@ export default async function migrateOmbul(pnPrisma: PrismaClientPn, vevenPrisma
                
             },
             create: {
-                id: ombul.id,
+                coverImage: {
+                    connect: {
+                        id: coverImage.id
+                    }
+                },
                 name: ombul.title,
                 description: ombul.lead,
                 createdAt: ombul.createdAt,
                 updatedAt: ombul.updatedAt,
                 year: ombul.year || 1919,
                 issueNumber: ombul.number,
-                fsLocation
+                fsLocation,
             }
         })
     }))
