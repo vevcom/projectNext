85f54124fb6e619bb692e139ab06103dc5d8f95b
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2024-03-05 21:44:52 +0100

    chore(feide-login): Remove unused files

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-res = requests.get('https://groups-api.dataporten.no/groups/groups', headers={
-    'Authorization': 'Bearer 1fbac21b-b953-41a8-b9e5-25575d733c5c'
-})
-
-print(res.status_code)
-print(res.text)
