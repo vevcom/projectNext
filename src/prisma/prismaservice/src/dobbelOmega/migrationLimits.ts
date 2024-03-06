2c9279c79479df8862d8cf6f1d5167b15cce6637
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-06 23:34:34 +0100

    chore: make getLimits a function

--- src/prisma/prismaservice/src/dobbelOmega/migrationLimits.ts
+++ src/prisma/prismaservice/src/dobbelOmega/migrationLimits.ts
@@ -1,7 +1,12 @@
 /**
  * Limits for the migration process to test without going crazy
+ * null means no limit
  */
-export const migrationLimits = {
-    numberOfCollectionsContainingImages: 3,
-    numberOfOmbuls: 7,
+export function getLimits() {
+    const limitsOn = process.env.MIGRATION_WITH_LIMITS !== 'false'
+    limitsOn ? console.log('Limimits on') : console.log('Limits off!!!!!!!!!!!!')
+    return {
+        ombul: limitsOn ? 5 : null,
+        numberOffFullImageCollections: limitsOn ? 3 : null,
+    }
 }
