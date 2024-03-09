1c44773d67c9f82bb748734b5a87093b4bd1f3cf
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2024-03-09 13:05:47 +0100

    page and scss

---
+++ src/app/lockers/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss'
+
+export default function Skapres() {
+    return (
+        <div className={styles.wrapper}>
+            <h1>Skapres</h1>
+        </div>
+    )
+}
