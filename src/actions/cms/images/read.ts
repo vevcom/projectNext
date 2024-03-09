38597d794de25093fb70627c7a1f2077ee14ffbc
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-09 12:40:03 +0100

    feat: adds ability to read Special cms images

--- src/actions/cms/images/read.ts
+++ src/actions/cms/images/read.ts
@@ -4,7 +4,13 @@ import prisma from '@/prisma'
 import errorHandler from '@/prisma/errorHandler'
 import type { ExpandedCmsImage } from './Types'
 import type { ActionReturn } from '@/actions/Types'
+import { SpecialCmsImage } from '@prisma/client'
 
+/**
+ * Read a cms image including the image
+ * @param name - name of the cms image the image
+ * @returns 
+ */
 export async function readCmsImage(name: string): Promise<ActionReturn<ExpandedCmsImage>> {
     //Note this action reates a image link if it does not exist and returns it
     try {
@@ -16,8 +22,43 @@ export async function readCmsImage(name: string): Promise<ActionReturn<ExpandedC
                 image: true,
             }
         })
+        if (!cmsImage) return { success: false, error: [{ message: `CmsImage ${name} not found` }] }
+        return { success: true, data: cmsImage }
+    } catch (error) {
+        return errorHandler(error)
+    }
+}
+
+/**
+ * Reads a special cmsImage, if it does not exist it creates it
+ * @param special SpecialCmsImage
+ * @returns ActionReturn<ExpandedCmsImage>
+ */
+export async function readSpecialCmsImage(special: SpecialCmsImage): Promise<ActionReturn<ExpandedCmsImage>> {
+    if (!Object.values(SpecialCmsImage).includes(special)) return {
+        success: false,
+        error: [{ message: `${special} is not special` }]
+    }
+    try {
+        const cmsImage = await prisma.cmsImage.findUnique({
+            where: {
+                name: special,
+            },
+            include: {
+                image: true,
+            }
+        })
         if (!cmsImage) {
-            return await createCmsImage(name)
+            const created = await prisma.cmsImage.create({
+                data: {
+                    name: special,
+                    special: special,
+                },
+                include: {
+                    image: true
+                }
+            })
+            return { success: true, data: created }
         }
         return { success: true, data: cmsImage }
     } catch (error) {
