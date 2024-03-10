3a6ac3a40ff9ea7b86c08ad397ebb3cd96e2bce7
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-10 21:43:36 +0100

    docs: document SpecialCmsParagraph

--- src/app/components/Cms/CmsParagraph/SpecialCmsParagraph.tsx
+++ src/app/components/Cms/CmsParagraph/SpecialCmsParagraph.tsx
@@ -7,6 +7,14 @@ type PropTypes = Omit<PropTypesCmsParapraph, 'cmsParagraph'> & {
     special: SpecialCmsParagraphT
 }
 
+/**
+ * WARNING: This component should only be rendered server side, on client fetch the paragraph using readSpecialCmsParagraph
+ * then render it using CmsParagraph.
+ * A component that reads a special type of paragraph using readSpecialCmsParagraph
+ * then renders it using standard CmsParagraph
+ * @param special - the special type of the paragraph to read
+ * @returns 
+ */
 export default async function SpecialCmsParagraph({ special, ...props }: PropTypes) {
     const res = await readSpecialCmsParagraph(special)
     if (!res.success) return <i>Error: {res.error && res.error[0].message}</i>
