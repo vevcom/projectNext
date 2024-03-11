eb04846c6bbfc0595e47778ebb00e20e8eb5e39c
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-11 17:14:24 +0100

    chore: adds expandedCommittee return and include Committee cover in the read return

---
+++ src/actions/groups/committees/Types.ts
@@ -0,0 +1,6 @@
+import type { Committee } from "@prisma/client"
+import type { ExpandedCmsImage } from "@/actions/cms/images/Types"
+
+export type ExpandedCommittee = Committee & {
+    logoImage: ExpandedCmsImage | null
+}
--- src/actions/groups/committees/read.ts
+++ src/actions/groups/committees/read.ts
@@ -1,14 +1,21 @@
 'use server'
 import { ActionReturn } from "@/actions/Types"
-import type { Committee } from "@prisma/client"
 import { createActionError, createPrismaActionError } from "@/actions/error"
 import prisma from "@/prisma"
+import { ExpandedCommittee } from "./Types"
 
-export default async function readCommitee(name: string) : Promise<ActionReturn<Committee>> {
+export default async function readCommitee(name: string) : Promise<ActionReturn<ExpandedCommittee>> {
     try {
         const committee = await prisma.committee.findUnique({
             where: {
                 name
+            },
+            include: {
+                logoImage: {
+                    include: {
+                        image: true
+                    }
+                }
             }
         })
         if (!committee) return createActionError('BAD PARAMETERS', "Committee not found")
