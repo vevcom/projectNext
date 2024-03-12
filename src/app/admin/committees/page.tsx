152b1834c9c3e499fb05d9ae2a3c08b74d463069
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-12 09:08:18 +0100

    chore: adds use of imagelist to adminCommittee

--- src/app/admin/committees/page.tsx
+++ src/app/admin/committees/page.tsx
@@ -1,11 +1,17 @@
 import Form from "@/app/components/Form/Form";
 import TextInput from "@/app/components/UI/TextInput";
 import create from "@/actions/groups/committees/create";
+import ImageSelectionProvider from "@/context/ImageSelection";
+import ImageList from "@/app/components/Image/ImageList/ImageList";
 
 export default function adminCommittee() {
     return (
-        <Form action={create.bind(null, 1)}>
-            <TextInput name="name" label="Navn"/>
-        </Form>
+        
+        <ImageSelectionProvider>
+            <ImageList />
+            <Form action={create.bind(null, 1)}>
+                <TextInput name="name" label="Navn"/>
+            </Form>
+        </ImageSelectionProvider>
     )
 }
