e707af1e01e2c05c0774993aabda19ccdd1b7688
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-12 12:42:40 +0100

    chore: grayscales backdropimage

--- src/app/committees/[name]/page.tsx
+++ src/app/committees/[name]/page.tsx
@@ -1,4 +1,6 @@
 import { readCommitee } from "@/actions/groups/committees/read"
+import { readSpecialImage } from "@/actions/images/read"
+import BackdropImage from "@/app/components/BackdropImage/BackdropImage"
 import { notFound } from "next/navigation"
 
 
@@ -12,6 +14,18 @@ export default async function Committee({ params } : PropTypes) {
     const name = decodeURIComponent(params.name)
     const res = await readCommitee(name)
     if (!res.success) notFound()
-    
-    return(params.name)
+    const committee = res.data
+
+    let committeeLogo = committee.logoImage.image
+    if (!committeeLogo) {
+        const res = await readSpecialImage('DAFAULT_COMMITTEE_LOGO')
+        if (!res.success) throw new Error('Kunne ikke finne standard komitelogo')
+        committeeLogo = res.data
+    }
+
+    return (
+        <BackdropImage image={committeeLogo}>
+            <h1>{committee.name}</h1>
+        </BackdropImage>
+    )
 }
--- src/app/components/BackdropImage/BackdropImage.tsx
+++ src/app/components/BackdropImage/BackdropImage.tsx
@@ -4,18 +4,24 @@ import Image from "../Image/Image";
 import styles from "./BackdropImage.module.scss"
 
 type PropTypes = { 
-    children:ReactNode
-    image:ImageT
- }
-
-export default function BackdropImage({ children, image }:PropTypes) {
+    children: ReactNode
+    image: ImageT
+    grayScale?: boolean
+}
+/**
+ * A component that renders a backdrop image with a content div on top of it
+ * @param children - The content to render on top of the image
+ * @param image - The image to render as a backdrop
+ * @param grayScale - Whether the image should be rendered in grayscale (true by default)
+ * */
+export default function BackdropImage({ children, image, grayScale = true }:PropTypes) {
     return (
         <div className={styles.BackdropImage}>
             <div className={styles.content}>
                 {children}
             </div>
             <div className={styles.image}>
-                <Image image={image} width={350} />
+                <Image className={styles.gray} image={image} width={350} />
             </div>
         </div>
     )
