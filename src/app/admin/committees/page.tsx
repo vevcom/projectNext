d35608328b5be42e0d657a2bb02f36212d897870
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-12 10:25:16 +0100

    chore: adds boilerplate for reeding ImageSelection correctly

--- src/app/admin/committees/page.tsx
+++ src/app/admin/committees/page.tsx
@@ -3,15 +3,33 @@ import TextInput from "@/app/components/UI/TextInput";
 import create from "@/actions/groups/committees/create";
 import ImageSelectionProvider from "@/context/ImageSelection";
 import ImageList from "@/app/components/Image/ImageList/ImageList";
+import ImagePagingProvider, { PageSizeImage } from "@/context/paging/ImagePaging";
+import { readSpecialImageCollection } from "@/actions/images/collections/read";
 
-export default function adminCommittee() {
+export default async function adminCommittee() {
+    const committeeLogoCollectionRes = await readSpecialImageCollection('COMMITEELOGOS')
+    if (!committeeLogoCollectionRes.success) throw new Error('Kunne ikke finne komitelogoer')
+    const { id: collectionId } = committeeLogoCollectionRes.data
+
+    const pageSize: PageSizeImage = 30
+
     return (
-        
-        <ImageSelectionProvider>
-            <ImageList />
-            <Form action={create.bind(null, 1)}>
-                <TextInput name="name" label="Navn"/>
-            </Form>
-        </ImageSelectionProvider>
+        <ImagePagingProvider 
+            serverRenderedData={[]} 
+            details={{
+                collectionId,
+            }}
+            startPage={{
+                page: 0,
+                pageSize,
+            }}
+        >
+            <ImageSelectionProvider>
+                <ImageList />
+                <Form action={create.bind(null, 1)}>
+                    <TextInput name="name" label="Navn"/>
+                </Form>
+            </ImageSelectionProvider>
+        </ImagePagingProvider>
     )
 }
