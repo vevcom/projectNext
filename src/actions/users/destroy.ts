91e674d30af42445c355b577906c9797cb2777f7
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-12 21:20:38 +0100

    refactor: destroyUser

--- src/actions/users/destroy.ts
+++ src/actions/users/destroy.ts
@@ -1,24 +1,14 @@
 'use server'
-import { createActionError, createPrismaActionError } from '@/actions/error'
-import prisma from '@/prisma'
 import type { ActionReturn } from '@/actions/Types'
 import type { User } from '@prisma/client'
+import { destroyUser } from '@/server/users/destroy'
 
-export async function destroyUser(id: number): Promise<ActionReturn<User>> {
-    try {
-        const user = await prisma.user.delete({
-            where: {
-                id,
-            },
-        })
-
-        if (!user) {
-            return createActionError('NOT FOUND', 'Bruker ikke funnet.')
-        }
-
-        return { success: true, data: user }
-    } catch (error) {
-        return createPrismaActionError(error)
-    }
-}
+/**
+ * Action to destroy a user by the given id
+ * @param id - The id of the user to destroy
+ * @returns 
+ */
+export async function destroyUserAction(id: number): Promise<ActionReturn<User>> {
+    return await destroyUser(id)
+}   
 
