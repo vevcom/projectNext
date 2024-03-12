b6d8d7ecdc148281638972ff568802cfea2aefec
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-12 10:53:34 +0100

    chore: adds CreateCommitteeForm component

---
+++ src/app/admin/committees/CreateCommitteeForm.tsx
@@ -0,0 +1,15 @@
+import styles from './CreateCommitteeForm.module.scss'
+
+import React from 'react'
+
+/**
+ * WARNING: The component expects to be rendered inside a ImageSelectionProvider, so the form can
+ * be submitted with a committee logo.
+ * A component to create a committee
+ * @returns committee form JSX
+ */
+export default function CreateCommitteeForm() {
+    return (
+        <div>CreateCommitteeForm</div>
+    )
+}
