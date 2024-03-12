f9dd55e9d8d7f80ff6826218250abe5559123a4c
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-12 14:25:09 +0100

    docs: for getCommittee

--- src/app/committees/[name]/getCommittee.ts
+++ src/app/committees/[name]/getCommittee.ts
@@ -2,6 +2,11 @@ import { notFound } from 'next/navigation'
 import { readCommitee } from '@/actions/groups/committees/read'
 import type { PropTypes } from './page'
 
+/**
+ * A function to get a committee from params in a page under /committees/[name]
+ * @param params - The name in an object resived by the page
+ * @returns 
+ */
 export default async function getCommitee(params: PropTypes['params']) {
     const name = decodeURIComponent(params.name)
     const res = await readCommitee(name)
