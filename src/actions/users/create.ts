5e7c763bf883246c201811213e8c6fe61244c07c
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-12 21:18:02 +0100

    docs: documents the functions for createUser

--- src/actions/users/create.ts
+++ src/actions/users/create.ts
@@ -8,6 +8,11 @@ import type { ActionReturn } from '@/actions/Types'
 import type { User } from '@prisma/client'
 import { createUser } from '@/server/users/create'
 
+/**
+ * A action that creates a user by the given data. It will also hash the password
+ * @param rawdata - The user to create
+ * @returns - The created user
+ */
 export async function createUserAction(rawdata: FormData | CreateUserSchemaType): Promise<ActionReturn<User>> {
     const parse = createUserSchema.safeParse(rawdata)
     if (!parse.success) {
--- src/server/users/create.ts
+++ src/server/users/create.ts
@@ -8,6 +8,11 @@ type CreateUserType = Omit<Prisma.UserCreateInput, 'passwordHash'> & {
     password: string
 }
 
+/**
+ * A action that creates a user by the given data. It will also hash the password
+ * @param data - The user to create
+ * @returns - The created user
+ */
 export async function createUser(data: CreateUserType): Promise<ActionReturn<User>> {
     const passwordHash = data.password //TODO: hash password
     try {
