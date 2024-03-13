ae504acb5180c87c86fc7191a5a6ddbd73929696
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-13 16:48:50 +0100

    chore: sets up some boilerplate for cms and imporves type alias

--- src/actions/cms/images/create.ts
+++ src/actions/cms/images/create.ts
@@ -2,7 +2,7 @@
 import prisma from '@/prisma'
 import { createPrismaActionError } from '@/actions/error'
 import type { Image } from '@prisma/client'
-import type { ExpandedCmsImage } from './Types'
+import type { ExpandedCmsImage } from '@/cms/images/Types'
 import type { ActionReturn } from '@/actions/Types'
 
 export async function createCmsImage(name: string, image?: Image): Promise<ActionReturn<ExpandedCmsImage>> {
