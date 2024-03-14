a9804e8b1c7147e526fac71d13b969a37e3b9a92
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-14 11:24:23 +0100

    refactor: read article category actions

--- src/actions/cms/articleCategories/read.ts
+++ src/actions/cms/articleCategories/read.ts
@@ -1,10 +1,6 @@
 'use server'
-import prisma from '@/prisma'
-import { createActionError, createPrismaActionError } from '@/actions/error'
 import type { ActionReturn } from '@/actions/Types'
-import type { Image } from '@prisma/client'
 import type {
-    ExpandedArticleCategory,
     ExpandedArticleCategoryWithCover,
     ArticleCategoryWithCover,
 } from '@/cms/articleCategories/Types'
@@ -14,7 +10,6 @@ export async function readArticleCategoriesAction(): Promise<ActionReturn<Articl
     //TODO: only read categories that user has visibility
     return await readArticleCategories()
 }
-
 
 export async function readArticleCategoryAction(name: string): Promise<ActionReturn<ExpandedArticleCategoryWithCover>> {
     //TODO: only read if right visibility
