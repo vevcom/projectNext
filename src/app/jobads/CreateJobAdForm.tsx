5af7505372f7115921d2a091ffc84a89cc433926
Gaute <gaute.flagstad@gmail.com>
2024-03-19 19:36:42 +0100

    feat: made jobAd form for creating jobads

---
+++ src/app/jobads/CreateJobAdForm.tsx
@@ -0,0 +1,27 @@
+'use client'
+import styles from './CreateJobAdForm.module.scss'
+import { createJobAd } from '@/actions/jobAds/create'
+import TextInput from '@/components/UI/TextInput'
+import Form from '@/components/Form/Form'
+import React from 'react'
+import { v4 as uuid } from 'uuid'
+import { useRouter } from 'next/navigation'
+
+export default function CreateJobAdForm() {
+    const { refresh } = useRouter()
+
+    return (
+        <div className={`${styles.CreateJobAdForm}`}>
+            <Form
+                title="Create a job ad"
+                submitText="Create job ad"
+                action={createJobAd}
+                successCallback={refresh}
+            >
+                <TextInput label="title" name="articleName" key={uuid()}/>
+                <TextInput label="company" name="company" key={uuid()}/>
+                <TextInput label="description" name="description" key={uuid()}/>
+            </Form>
+        </div>
+    )
+}
