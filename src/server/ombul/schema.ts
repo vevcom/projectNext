55122346d0a49c22cc3346a7a264e5c7bf6e1cd3
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-19 18:00:44 +0100

    chore: start work on ombul

--- src/server/ombul/schema.ts
+++ src/server/ombul/schema.ts
@@ -2,8 +2,18 @@ import { maxOmbulFileSize } from '@/server/ombul/ConfigVars'
 import { imageFileSchema } from '@/actions/images/schema'
 import { z } from 'zod'
 import { zfd } from 'zod-form-data'
+import { Validation } from '../Validation'
 
-export const ombulSchema = z.object({
+export const baseOmbulValidation = new Validation({
+    ombulFile: z.instanceof(File),
+    ombulCoverImage: imageFileSchema,
+    year: z.string().transform(val => parseInt(val, 10))
+        .refine(val => val >= 1900 && val <= new Date().getFullYear(), 'År må være mellom 1900 og nå').optional(),
+    issueNumber: z.string().transform(val => parseInt(val, 10))
+        .refine(val => val <= 30, 'max 30').optional(),,
+    name: z.string(),
+    description: z.string(),
+}, {
     ombulFile: z.instanceof(File).refine(file => file.size < maxOmbulFileSize, 'Fil må være mindre enn 10mb'),
     ombulCoverImage: imageFileSchema,
     year: z.string().transform(val => parseInt(val, 10))
@@ -14,8 +24,6 @@ export const ombulSchema = z.object({
     description: z.string().min(2, 'Minimum lengde er 2').max(100, 'Maximum lengde er 100').trim()
 })
 
-export const createOmbulSchema = zfd.formData(ombulSchema)
-export type CreateOmbulSchemaType = z.infer<typeof createOmbulSchema>
 
 export const updateOmbulSchema = zfd.formData(
     ombulSchema.partial().omit({
