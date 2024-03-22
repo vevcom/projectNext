cc4307070197e9c509e548a65ae5e98a03ea3153
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-22 12:54:55 +0100

    docs: destroVisibility

--- src/server/visibility/destroy.ts
+++ src/server/visibility/destroy.ts
@@ -2,6 +2,11 @@ import 'server-only'
 import prisma from '@/prisma'
 import { prismaCall } from '../prismaCall'
 
+/**
+ * Destroy a visibility. This will also destroy the visibility levels associated with the visibility,
+ * to prevent orphaned visibility levels.
+ * @param id - The id of the visibility to destroy
+ */
 export async function destroyVisibility(id: number) : Promise<void> {
     const visibility = await prismaCall(() => prisma.visibility.delete({
         where: {
