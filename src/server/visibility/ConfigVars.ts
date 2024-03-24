6a3e50263a754a3d8f647dcf24d592d6a591e82e
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-24 20:16:47 +0100

    chore: adds config for buypass permissions

---
+++ src/server/visibility/ConfigVars.ts
@@ -0,0 +1,13 @@
+import { Permission } from "@prisma/client";
+
+/**
+ * These are the permissions that buypass the visibiity system. I-e for reading images
+ * if the user has the permission it does not matter if the visibility is fullfilled. 
+ */
+const BuypassPermissions = {
+    images: 'IMAGE_ADMIN',
+    cms: 'CMS_ADMIN',
+    events: 'EVENT_ADMIN'
+} as const satisfies { [key: string]: Permission }
+
+type BuyPassPermissions = typeof BuypassPermissions[keyof typeof BuypassPermissions]
