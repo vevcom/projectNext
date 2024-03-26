50e33f34bc95af3cf613f96477bf00e98555c6ef
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-26 12:37:46 +0100

    chore: display omegaOrder on page

--- src/app/admin/stateOfOmega/page.tsx
+++ src/app/admin/stateOfOmega/page.tsx
@@ -1,9 +1,20 @@
+import { getUser } from '@/auth/getUser'
 import styles from './page.module.scss'
+import { notFound } from 'next/navigation'
+import { readCurrentOmegaOrderAction } from '@/actions/omegaOrder/read'
 
-export default function stateOfOmega() {
+export default async function stateOfOmega() {
+    const { authorized } = await getUser({
+        requiredPermissions: [['OMEGA_ORDER_CREATE']]
+    })
+    if (!authorized) return notFound() //TODO: improve error handling
+
+    const currentOreder = await readCurrentOmegaOrderAction()
+    if (!currentOreder.success) return notFound() //TODO: improve error handling
+
     return (
         <div className={styles.wrapper}>
-            
+            <h1>Omega er i orden: { currentOreder.data.order }</h1>
         </div>
     )
 }
