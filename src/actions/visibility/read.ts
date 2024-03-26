a8cfa47ed0e2b48c3ac5be22205fe18f3addc39f
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-26 16:24:48 +0100

    chore: adds some creazy logic to decode groupMatricies for client

--- src/actions/visibility/read.ts
+++ src/actions/visibility/read.ts
@@ -4,16 +4,20 @@ import { safeServerCall } from "../safeServerCall"
 import { readGroupsStructured } from "@/server/groups/read"
 import { ActionReturn } from "../Types"
 import { createActionError } from "../error"
-import { GroupType, VisibilityPurpose, VisibilityType } from "@prisma/client"
+import { GroupType } from "@prisma/client"
 import { checkVisibility } from "@/auth/checkVisibility"
 import { getUser } from "@/auth/getUser"
 import { ExpandedGroup, GroupsStructured } from "@/server/groups/Types"
 import { PurposeTextsConfig } from "@/server/visibility/ConfigVars"
 import { GroupMatrix } from "@/server/visibility/Types"
+import { GroupTypesConfig } from "@/server/groups/ConfigVars"
 
-type VisibilityRequiermentForAdmin = (ExpandedGroup & {
-    selected: boolean
-})[]
+type VisibilityRequiermentForAdmin = {
+    name: string
+    groups: (ExpandedGroup & {
+        selected: boolean
+    })[]
+}
 
 type VisibilityStructuredForAdmin = {
     purpose: string
@@ -72,7 +76,60 @@ const standardGroupings = ['CLASS','OMEGA_MEMBERSHIP_GROUP', 'STUDY_PROGRAMME'] 
 function expandOneLevel(matrix: GroupMatrix, groups: GroupsStructured) : VisibilityRequiermentForAdmin[] {
     const res : VisibilityRequiermentForAdmin[] = []
     standardGroupings.forEach(groupType => {
+        const standardRequriment : VisibilityRequiermentForAdmin = {
+            name: GroupTypesConfig[groupType].name,
+            groups: groups[groupType].groups.map(group => ({
+                ...group,
+                selected: false
+            }))
+        }
+        //Find out if there is a row in the matrix with just the group type.
+        for (let i = 0; i < matrix.length; i++) {
+            if (matrix[i].every(groupId => groupTypeOfId(groupId, groups) === groupType)) {
+                standardRequriment.groups.forEach(group => {
+                    group.selected = matrix[i].includes(group.id)
+                })
+                //Remove the row from the matrix since it has been handled
+                matrix.splice(i, 1)
+            }
+        }
+        res.push(standardRequriment)
+    })
+
+    //Handle all non standard groupings
+    matrix.forEach(row => {
+        const groups_ = row.reduce((acc, id) => {
+            const g = findGroupOfId(id, groups)
+            if (g) acc.push(g)
+            return acc
+        }, [] as ExpandedGroup[])
+        const nonStandardRequriment : VisibilityRequiermentForAdmin = {
+            name: 'ekstra',
+            groups: groups_.map(g => ({
+                ...g,
+                selected: true
+            }))
+        }
+        res.push(nonStandardRequriment)
     })
 
     return res
+}
+
+function findGroupOfId(id: number, groups: GroupsStructured) : ExpandedGroup | null {
+    Object.keys(groups).forEach(groupType => {
+        groups[groupType].groups.forEach(group => {
+            if (group.id === id) return group
+        })
+    })
+    return null
+}
+
+function groupTypeOfId(id: number, groups: GroupsStructured) : GroupType {
+    Object.keys(groups).forEach(groupType => {
+        groups[groupType].groups.forEach(group => {
+            if (group.id === id) return group.groupType
+        })
+    })
+    return 'MANUAL_GROUP'
 }
