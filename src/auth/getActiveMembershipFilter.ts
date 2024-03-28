d614b729622c7efa9282e77cf00d0da7cfaef6f6
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-03-28 12:58:02 +0100

    chore: modify getActiveMembershipFilter

--- src/auth/getActiveMembershipFilter.ts
+++ src/auth/getActiveMembershipFilter.ts
@@ -2,36 +2,15 @@ import type { Prisma } from '@prisma/client'
 
 /**
  * A function to return a filter that only selects valid memberships of a order.
- * @param order - The order of what is considered valid membership.
- * @param strictness - If 'ACTIVE' all memberships that are valid will be returned (default).
- * If exact only mebership that matches the exact order will be returned. This only matters
- * if the group has membershipRenewal. I.e OMEGA_MEMBER group has renewal and will return all
- * members of all orders less than or equal to the given order if 'ACTIVE' is used. If 'EXACT'
- * is used the membership must be exanctly the given order, even if the membership is valid.
+ * @param order - The order of what is considered valid membership. Or 'ACTIVE' to only select active memberships.
  * @returns - A filter that only selects valid memberships of a order.
  */
 export function getActiveMembershipFilter(
-    order: number,
-    strictness: 'ACTIVE' | 'EXACT' = 'ACTIVE'
+    order: number | 'ACTIVE',
 ) {
-    return {
-        OR: [
-            {
-                group: {
-                    membershipRenewal: false,
-                },
-                order,
-            },
-            {
-                group: {
-                    membershipRenewal: true,
-                },
-                order: strictness === 'ACTIVE' ? {
-                    lte: order,
-                } : {
-                    equals: order,
-                },
-            }
-        ]
+    return order === 'ACTIVE' ? {
+        active: true,
+    } : {
+        order: order,
     } satisfies Prisma.MembershipWhereInput
 }
