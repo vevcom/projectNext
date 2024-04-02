2efb62343389ad421eca5a974ee80e5cee0ff64b
Gaute <gaute.flagstad@gmail.com>
2024-04-02 19:18:26 +0200

    feat: made actions for reading a job ad, and multiple job ads

---
+++ src/actions/jobAds/read.ts
@@ -0,0 +1,19 @@
+'use server'
+import { safeServerCall } from '@/actions/safeServerCall'
+import { readJobAdByNameAndOrder, readJobAdsCurrent } from '@/server/jobAds/read'
+import type { ActionReturn, ReadPageInput } from '@/actions/Types'
+import { ExpandedJobAd, SimpleJobAd } from '@/server/jobAds/Types'
+
+
+export async function readJobAdsCurrentAction(): Promise<ActionReturn<SimpleJobAd[]>> {
+    //TODO: only read news with right visibility
+    return await safeServerCall(() => readJobAdsCurrent())
+}
+
+export async function readJobAdAction(idOrName: number | {
+    articleName: string
+    order: number
+}): Promise<ActionReturn<ExpandedJobAd>> {
+
+    return await safeServerCall(() => readJobAdByNameAndOrder(idOrName))
+}
