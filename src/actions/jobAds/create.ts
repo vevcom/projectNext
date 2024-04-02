d7838f3388e1a63b22251ddd2a48f4b141e0f595
Gaute <gaute.flagstad@gmail.com>
2024-04-02 19:17:35 +0200

    fix: moved logic to server folder

--- src/actions/jobAds/create.ts
+++ src/actions/jobAds/create.ts
@@ -1,46 +1,19 @@
 'use server'
-import { createJobAdSchema } from './schema'
-import { createArticle } from '@/cms/articles/create'
-import prisma from '@/prisma'
-import { createPrismaActionError, createActionError, createZodActionError } from '@/actions/error'
-import type { JobAd } from '@prisma/client'
+import { safeServerCall } from '@/actions/safeServerCall'
+import { createZodActionError } from '@/actions/error'
+import { createJobAdValidation } from '@/server/jobAds/validation'
 import type { ActionReturn } from '@/actions/Types'
-import type { JobAdSchemaType } from './schema'
-import { read } from 'fs'
-import { readCurrenOmegaOrder } from '../omegaOrder/read'
+import type { CreateJobAdTypes } from '@/server/jobAds/validation'
+import { ExpandedJobAd } from '@/server/jobAds/Types'
+import { createJobAd } from '@/server/jobAds/create'
 
-export async function createJobAd(rawdata: FormData | JobAdSchemaType): Promise<ActionReturn<JobAd>> {
-    const parse = createJobAdSchema.safeParse(rawdata)
-    if (!parse.success) {
-        return createZodActionError(parse)
-    }
-
+export async function createJobAdAction(
+    rawdata: FormData | CreateJobAdTypes['Type']
+): Promise<ActionReturn<ExpandedJobAd>> {
+    //TODO: check for can create news permission
+    const parse = createJobAdValidation.typeValidate(rawdata)
+    if (!parse.success) return createZodActionError(parse)
     const data = parse.data
 
-    const articleRes = await createArticle(data.articleName)
-    if (!articleRes.success) return articleRes
-
-    const currentOrder = await readCurrenOmegaOrder()
-    if (!currentOrder.success) return currentOrder
-    try {
-        const jobAds = await prisma.jobAd.create({
-            data: {
-                company: data.company,
-                description: data.description,
-                article: {
-                    connect: {
-                        id: articleRes.data.id
-                    }
-                },
-                omegaOrder: {
-                    connect: {
-                        order: currentOrder.data.order
-                    }
-                }
-            }
-        })
-        return { success: true, data: jobAds }
-    } catch (error) {
-        return createPrismaActionError(error)
-    }
+    return await safeServerCall(() => createJobAd(data))
 }
