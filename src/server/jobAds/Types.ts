2848fdf695841d2453512c89d9eea82d2f3381db
Gaute <gaute.flagstad@gmail.com>
2024-04-02 19:26:33 +0200

    feat: added a type for simple job ads, for reading many

--- src/server/jobAds/Types.ts
+++ src/server/jobAds/Types.ts
@@ -1,5 +1,11 @@
 import type { ExpandedArticle } from '../cms/articles/Types'
 import type { JobAd } from '@prisma/client'
+import type { Image } from '@prisma/client'
 export type ExpandedJobAd = JobAd & {
     article: ExpandedArticle
+}
+
+//used for read many actions
+export type SimpleJobAd = JobAd & {
+    coverImage: Image | null
 }
