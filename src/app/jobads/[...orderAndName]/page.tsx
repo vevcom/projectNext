6bdaa2caca45b3b4c52410ad164e8f8ad604fc35
Gaute <gaute.flagstad@gmail.com>
2024-04-02 19:21:27 +0200

    fix: job ad page now properly shows the content of the ad

--- src/app/jobads/[...orderAndName]/page.tsx
+++ src/app/jobads/[...orderAndName]/page.tsx
@@ -1,15 +1,30 @@
+
+import { notFound } from 'next/navigation'
 import styles from './page.module.scss'
+import { readJobAdAction } from '@/actions/jobAds/read'
+import Article from '@/app/components/Cms/Article/Article'
 type PropTypes = {
     params: {
-        name: string
+        orderAndName: string[]
     }
 }
 
 
-export default function JobAd({ params }: PropTypes) {
+export default async function JobAd({ params }: PropTypes) {
+    if (params.orderAndName.length !== 2) notFound()
+    const order = parseInt(decodeURIComponent(params.orderAndName[0]))
+    const name = decodeURIComponent(params.orderAndName[1])
+
+    const jobAdRes = await readJobAdAction({ articleName: name, order })
+    if (!jobAdRes.success) {
+        if (jobAdRes.errorCode === "NOT FOUND") notFound()
+        throw new Error('Failed to read jobAd')
+    }
+    const jobAd = jobAdRes.data
     return (
         <div className={styles.wrapper}>
-            <h1>JobAd {params.name}</h1>
+            <Article article={jobAd.article} />
         </div>
+
     )
 }
