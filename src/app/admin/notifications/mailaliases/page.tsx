5ce269c20b18ee25c5820dabc0322d48a05ff633
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2024-04-06 14:14:16 +0200

    chore(mailalias): Hide add alias button if the user is not allowed to add an alias

--- src/app/admin/notifications/mailaliases/page.tsx
+++ src/app/admin/notifications/mailaliases/page.tsx
@@ -1,18 +1,25 @@
 import AddHeaderItemPopUp from "@/app/components/AddHeaderItem/AddHeaderItemPopUp";
 import PageWrapper from "@/app/components/PageWrapper/PageWrapper";
 import CreateMailAlias from "./createAliasForm";
+import { getUser } from "@/auth/getUser";
 
 
-export default function mailAliases() {
+export default async function mailAliases() {
 
+    const { permissions } = await getUser({
+        userRequired: true,
+        shouldRedirect: true,
+    })
+
+    const showAddButton = permissions.includes("MAILALIAS_CREATE")
 
     return (
         <PageWrapper
             title="Elektronisk post alias"
             headerItem={
-                <AddHeaderItemPopUp PopUpKey="createMailAlias">
+                showAddButton ? <AddHeaderItemPopUp PopUpKey="createMailAlias">
                     <CreateMailAlias/>
-                </AddHeaderItemPopUp>
+                </AddHeaderItemPopUp> : null
             }
         >
             Her kommer det en liste på sikt
