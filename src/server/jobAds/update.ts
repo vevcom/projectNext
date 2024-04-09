dade724b2fb89f86d9ab93a7c6ae094a99e2cf62
Gaute <gaute.flagstad@gmail.com>
2024-04-09 17:28:43 +0200

    feat: added function for updating job ad poster on job ads site

---
+++ src/server/jobAds/update.ts
@@ -0,0 +1,24 @@
+import 'server-only'
+import { updateJobAdValidation } from './validation'
+import { prismaCall } from '@/server/prismaCall'
+import prisma from '@/prisma'
+import type { UpdateJobAdTypes } from './validation'
+import { SimpleJobAd } from './Types'
+
+export async function updateJobAd(
+    id: number,
+    rawdata: UpdateJobAdTypes['Detailed']
+): Promise<Omit<SimpleJobAd, 'coverImage'>> {
+    const data = updateJobAdValidation.detailedValidate(rawdata)
+    return await prismaCall(() => prisma.jobAd.update({
+        where: { id },
+        data: {
+            description: data.description,
+            article: {
+                update: {
+                    name: data.articleName
+                }
+            }
+        }
+    }))
+}
