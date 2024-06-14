5d5771e7019ff4380220e2055da3c6cef92219d2
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2024-06-14 13:02:37 +0200

    fix(lockers): toggling indefinateDate no longer resets groupId

--- src/app/lockers/[id]/UpdateLockerReservationForm.tsx
+++ src/app/lockers/[id]/UpdateLockerReservationForm.tsx
@@ -9,15 +9,20 @@ import { useState } from 'react'
 
 type PropTypes = {
     reservationId: number,
-    committeesFormData: {
+    groupsFormData: {
         value: string,
         label: string
     }[]
 }
 
-export default function UpdateLockerReservationForm({ reservationId, committeesFormData }: PropTypes) {
+export default function UpdateLockerReservationForm({ reservationId, groupsFormData }: PropTypes) {
     const { refresh } = useRouter()
     const [indefinateDate, setIndefinateDate] = useState(false)
+    const [groupId, setGroupId] = useState("-1") // State on groupId is used to prevent form from resetting when indefinateDate is toggled
+
+    function handleGroupIdChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        setGroupId(event.target.value)
+    }
 
     return (
         <Form 
@@ -26,8 +31,18 @@ export default function UpdateLockerReservationForm({ reservationId, committeesF
             submitText="Oppdater" 
             action={updateLockerReservationAction.bind(null, reservationId)}
         >   
-            <Select label="Reserver for" name="groupId" options={[{value: "-1", label: "Meg selv"}, ...committeesFormData]} />
-            <Checkbox label="Reserver på ubestemt tid" name="indefinateDate" onChange={() => setIndefinateDate(!indefinateDate)}/>
+            <Select
+                label="Reserver for" 
+                name="groupId"
+                value={groupId}
+                options={[{value: "-1", label: "Meg selv"}, ...groupsFormData]} 
+                onChange={handleGroupIdChange}
+            />
+            <Checkbox
+                label="Reserver på ubestemt tid" 
+                name="indefinateDate"
+                onChange={() => setIndefinateDate(!indefinateDate)}
+            />
             {!indefinateDate && <DateInput label="Reserver fram til" name="endDate"/>}
         </Form>
     )
