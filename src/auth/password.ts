432d9c99037a55784b95176a015ec489e00b75e9
Paulius Juzenas <pauliusj@stud.ntnu.no>
2024-06-20 22:52:46 +0200

    style: fix linting :/

--- src/auth/password.ts
+++ src/auth/password.ts
@@ -4,7 +4,7 @@ const saltRounds = 12
 
 /**
  * Wrapper for the bcrypt `hash` function.
- * 
+ *
  * @param password The password to be hashed.
  * @returns Hashed password string.
  */
@@ -14,7 +14,7 @@ export async function hashPassword(password: string) {
 
 /**
  * Wrapper for the bcrypt `compare` function.
- * 
+ *
  * @param password The unhashed password to check.
  * @param passwordHash The password hash to compare against.
  * @returns `true` if the password matches, else `false`.
