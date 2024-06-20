47bb9c43f7409c4b5f5a854f101f5ff0bf43eaf7
Paulius Juzenas <pauliusj@stud.ntnu.no>
2024-06-20 22:49:21 +0200

    docs: add jsdocs to password functions

--- src/auth/password.ts
+++ src/auth/password.ts
@@ -2,10 +2,23 @@ import bcrypt from 'bcrypt'
 
 const saltRounds = 12
 
+/**
+ * Wrapper for the bcrypt `hash` function.
+ * 
+ * @param password The password to be hashed.
+ * @returns Hashed password string.
+ */
 export async function hashPassword(password: string) {
     return bcrypt.hash(password, saltRounds)
 }
 
+/**
+ * Wrapper for the bcrypt `compare` function.
+ * 
+ * @param password The unhashed password to check.
+ * @param passwordHash The password hash to compare against.
+ * @returns `true` if the password matches, else `false`.
+ */
 export async function comparePassword(password: string, passwordHash: string) {
     return bcrypt.compare(password, passwordHash)
 }
