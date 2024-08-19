3a4db41649285a54e71dafacabfe710ae15c2eef
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2024-08-19 15:06:58 +0200

    chore: change the from in the mail senidng form to a select

--- src/app/admin/send-mail/mailForm.tsx
+++ src/app/admin/send-mail/mailForm.tsx
@@ -2,18 +2,31 @@
 import styles from './mailForm.module.scss'
 import sendMail from '@/actions/sendmail/send'
 import Form from '@/app/components/Form/Form'
+import Select from '@/app/components/UI/Select'
 import TextInput from '@/app/components/UI/TextInput'
 import Textarea from '@/app/components/UI/Textarea'
+import { MailAlias } from '@prisma/client'
 
 
-export default function MailForm() {
+export default function MailForm({
+    aliases,
+}: {
+    aliases: MailAlias[]
+}) {
     return <Form
         action={sendMail}
         submitText="Send elektronisk post"
         className={styles.mailForm}
         buttonClassName={styles.mailButton}
     >
-        <TextInput name="from" label="Avsender"/>
+        <Select
+            name="from"
+            label="Avsender"
+            options={aliases.map(alias => ({
+                value: alias.address
+            }))}
+            className={styles.select}
+        />
         <TextInput name="to" label="Mottaker"/>
         <TextInput name="subject" label="Emne"/>
 
--- src/app/admin/send-mail/page.tsx
+++ src/app/admin/send-mail/page.tsx
@@ -2,6 +2,7 @@ import MailForm from './mailForm'
 import PageWrapper from '@/components/PageWrapper/PageWrapper'
 import { getUser } from '@/auth/getUser'
 import { notFound } from 'next/navigation'
+import { readMailAliasesAction } from '@/actions/mail/alias/read'
 
 export default async function SendMail() {
     // TODO: permission checks
@@ -9,13 +10,15 @@ export default async function SendMail() {
         requiredPermissions: [['MAIL_SEND']],
     })
 
-    if (!authorized) {
+    const mailAliases = await readMailAliasesAction()
+
+    if (!authorized || !mailAliases.success) {
         notFound()
     }
 
     return (
         <PageWrapper title="Elektronisk postutsendelse">
-            <MailForm />
+            <MailForm aliases={mailAliases.data} />
         </PageWrapper>
     )
 }
