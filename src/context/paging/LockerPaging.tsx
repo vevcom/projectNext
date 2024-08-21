ccfc71ca2016bb6dbad96a98e8c074604029cad3
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2024-08-21 14:04:33 +0200

    chore(lockers): LockerCursor

--- src/context/paging/LockerPaging.tsx
+++ src/context/paging/LockerPaging.tsx
@@ -2,12 +2,12 @@
 import generatePagingProvider, { generatePagingContext } from '@/context/paging/PagingGenerator'
 import { readLockerPageAction } from '@/actions/lockers/read'
 import type { ReadPageInput } from '@/server/paging/Types'
-import type { LockerWithReservation } from '@/server/lockers/Types'
+import type { LockerCursor, LockerWithReservation } from '@/server/lockers/Types'
 
 export type PageSizeLocker = 20
-const fetcher = async (x: ReadPageInput<PageSizeLocker>) => await readLockerPageAction(x)
+const fetcher = async (x: ReadPageInput<PageSizeLocker, LockerCursor>) => await readLockerPageAction(x)
 
-export const LockerPagingContext = generatePagingContext<LockerWithReservation, PageSizeLocker>()
+export const LockerPagingContext = generatePagingContext<LockerWithReservation, LockerCursor, PageSizeLocker>()
 const LockerPagingProvider = generatePagingProvider({ 
     Context: LockerPagingContext,
     fetcher,
--- src/server/lockers/Types.ts
+++ src/server/lockers/Types.ts
@@ -2,3 +2,7 @@ import type { Prisma } from '@prisma/client'
 import { readLocker } from './read'
 
 export type LockerWithReservation = Prisma.PromiseReturnType<typeof readLocker>
+
+export type LockerCursor = {
+    id: number
+} 
