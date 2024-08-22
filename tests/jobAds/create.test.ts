0f27d16909cd12c1195bef51d15b99709ed2e44c
Paulius Juzenas <pauliusj@stud.ntnu.no>
2024-08-22 22:17:59 +0200

    feat: basic test for job ads create

---
+++ tests/jobAds/create.test.ts
@@ -0,0 +1,40 @@
+import { createJobAdAction } from '@/actions/jobAds/create'
+import { getServerSession } from 'next-auth'
+import { vi, it, describe, expect } from 'vitest'
+import { mockUser, setMockSession } from 'tests/mockSession'
+
+const testJobAd = {
+    articleName: 'Next JS utvikler',
+    description: 'Vi ser etter utviklere som er glade i webutvikling og kake.',
+    company: 'Vevcom',
+}
+
+describe('Job Ads', () => {
+    it('Create Job Ad with Unauthorized User', async () => {
+        setMockSession({
+            memberships: [],
+            permissions: [],
+            user: mockUser,
+        })
+
+        const createdJobAdRes = await createJobAdAction(testJobAd)
+
+        expect(createdJobAdRes).toMatchObject({ success: false })
+    })
+
+    it('Create Job Ad with Authorized User', async () => {
+        setMockSession({
+            permissions: ['JOBAD_CREATE']
+        })
+
+        vi.mocked(getServerSession).mockResolvedValue({ 
+            user: mockUser,
+            permissions: ['CREATE_PERMISSION'],
+            memberships: [],
+        })
+
+        const createdJobAdRes = await createJobAdAction(testJobAd)
+
+        expect(createdJobAdRes).toMatchObject({ success: false })
+    })
+})
