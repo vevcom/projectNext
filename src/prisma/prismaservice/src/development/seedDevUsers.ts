55078e8112f08dfec476d5e440c79edbe0dadd69
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-08-22 13:40:02 +0200

    chore: adds accepted terms to harambe on seeding

--- src/prisma/prismaservice/src/development/seedDevUsers.ts
+++ src/prisma/prismaservice/src/development/seedDevUsers.ts
@@ -79,7 +79,8 @@ export default async function seedDevUsers(prisma: PrismaClient) {
                 connect: {
                     name: 'Harambe104'
                 }
-            }
+            },
+            acceptedTerms: new Date(),
         },
     })
 
