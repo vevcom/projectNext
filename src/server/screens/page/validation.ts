eb8fe9f30619a369b7ac5a9c9ddf8fa7e1c80421
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-08-22 16:38:07 +0200

    chore: adds validation for the screen api

---
+++ src/server/screens/page/validation.ts
@@ -0,0 +1,29 @@
+import { ValidationBase } from '@/server/Validation'
+import { z } from 'zod'
+import { ScreenPageType } from '@prisma/client'
+import type { ValidationTypes } from '@/server/Validation'
+
+const basePageValidation = new ValidationBase({
+    type: {
+        name: z.string(),
+        type: z.nativeEnum(ScreenPageType),
+        connectToJobAd: z.string()
+    },
+    details: {
+        name: z.string(),
+        type: z.nativeEnum(ScreenPageType),
+        connectToJobAd: z.number()
+    }
+})
+
+export const createPageValidation = basePageValidation.createValidation({
+    keys: ['name'],
+    transformer: data => data
+})
+export type CreatePageTypes = ValidationTypes<typeof createPageValidation>
+
+export const updatePageValidation = basePageValidation.createValidation({
+    keys: ['name', 'type', 'connectToJobAd'],
+    transformer: data => ({ ...data, connectToJobAd: parseInt(data.connectToJobAd) })
+})
+export type UpdatePageTypes = ValidationTypes<typeof updatePageValidation>
