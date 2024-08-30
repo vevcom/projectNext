2afb42cbf61c8b64c0797fd23095efccce5fe132
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-08-30 22:37:23 +0200

    chore: adds basic landing page for fagveven

--- src/app/education/schools/page.tsx
+++ src/app/education/schools/page.tsx
@@ -1,9 +1,21 @@
+import styles from './page.module.scss'
+import { getUser } from '@/auth/getUser'
 import PageWrapper from '@/components/PageWrapper/PageWrapper'
+import Link from 'next/link'
 
+export default async function Schools() {
+    const { permissions } = await getUser()
 
-export default function Schools() {
+    const isSchoolAdmin = permissions.includes('SCHOOLS_ADMIN')
+
     return (
-        <PageWrapper title="Skoler">
+        <PageWrapper title="Skoler" headerItem={
+            isSchoolAdmin ? (
+                <Link href="/admin/schools" className={styles.adminLink}>
+                    Gå til administrasjon
+                </Link>
+            ) : <></>
+        }>
             <p>Dette er en liste skole. Du er velkommen til å legge til flere</p>
         </PageWrapper>
     )
