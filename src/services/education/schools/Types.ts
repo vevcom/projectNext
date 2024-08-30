3c9ba2dda89f17d125df6fc2f71b23adb74730d0
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-08-30 23:56:49 +0200

    feat: adds cms link to school for linking to school homepage

--- src/services/education/schools/Types.ts
+++ src/services/education/schools/Types.ts
@@ -1,12 +1,13 @@
 import type { ExpandedCmsImage } from '@/cms/images/Types'
 import type { SchoolFieldsToExpose } from './ConfigVars'
-import type { CmsParagraph, School } from '@prisma/client'
+import type { CmsLink, CmsParagraph, School } from '@prisma/client'
 
 export type SchoolFiltered = Pick<School, typeof SchoolFieldsToExpose[number]>
 
 export type ExpandedSchool = SchoolFiltered & {
     cmsImage: ExpandedCmsImage,
     cmsParagraph: CmsParagraph,
+    cmsLink: CmsLink,
 }
 
 export type SchoolCursor = {
