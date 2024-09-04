422e16664d0c6e1fe91b48be67f8eb61c40fb2c0
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-09-04 23:17:49 +0200

    chore: adds basic tables for applications

---
+++ src/app/applications/[periodName]/page.tsx
@@ -0,0 +1,9 @@
+import PageWrapper from "@/components/PageWrapper/PageWrapper"
+
+export async function ApplicationPeriod() {
+    return (
+        <PageWrapper title="Søknad">
+            Søknad
+        </PageWrapper>
+    )
+}
