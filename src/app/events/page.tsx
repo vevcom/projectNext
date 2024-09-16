c77458bd654eb4336db51e295653134e2dca0c1b
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-09-16 22:10:58 +0200

    chore: adds title to /evens landing page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,9 +1,14 @@
+import PageWrapper from '../_components/PageWrapper/PageWrapper'
 import styles from './page.module.scss'
 
 export default function Events() {
     return (
         <div className={styles.wrapper}>
-            events
+            <h1>Hvad Der Hender</h1>
+            <ul>   
+                <li>
+                </li>
+            </ul>
         </div>
     )
 }
