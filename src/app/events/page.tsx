1bb429b70c7644f3ac8a81df74eb77f9930f2c21
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-09-17 21:41:44 +0200

    chore: adds styling to /events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -3,6 +3,9 @@ import CreateEventForm from './CreateEventForm'
 import { AddHeaderItemPopUp } from '@/components/HeaderItems/HeaderItemPopUp'
 import { readCurrentEvents } from '@/actions/events/read'
 import EventCard from '@/components/Event/EventCard'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArchive, faTag } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
 
 export default async function Events() {
     const currentEventsResponse = await readCurrentEvents()
@@ -13,19 +16,30 @@ export default async function Events() {
 
     return (
         <div className={styles.wrapper}>
-            <h1>Hvad Der Hender</h1>
-            <AddHeaderItemPopUp PopUpKey="CreateEventPopUp">
-                <div className={styles.createEvent}>
-                    <CreateEventForm />
+            <div className={styles.top}>
+                <h1>Hvad Der Hender</h1>
+                <div className={styles.navButtons}>
+                    <Link href="/admin/eventtags">
+                        <FontAwesomeIcon icon={faTag} />
+                    </Link>
+                    <Link href="/events/archive">
+                        <FontAwesomeIcon icon={faArchive} />
+                    </Link>
+                    <AddHeaderItemPopUp PopUpKey="CreateEventPopUp">
+                        <div className={styles.createEvent}>
+                            <CreateEventForm />
+                        </div>
+                    </AddHeaderItemPopUp>
                 </div>
-            </AddHeaderItemPopUp>
-            <ul>
+            </div>
+            
+            <main>
             {
                 currentEvents.map(event => 
                     <EventCard event={event} />
                 )
             }
-            </ul>
+            </main>
         </div>
     )
 }
