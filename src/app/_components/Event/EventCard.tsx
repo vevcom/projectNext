9cecb6bd70c068986aeb03d1dcd89ebf21bced34
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-09-18 18:19:02 +0200

    chore: makes EventCards more custom

--- src/app/_components/Event/EventCard.tsx
+++ src/app/_components/Event/EventCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ImageCard from '../ImageCard/ImageCard'
 import { ExpandedCmsImage } from '@/services/cms/images/Types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendar, faUsers } from '@fortawesome/free-solid-svg-icons'
 import styles from './EventCard.module.scss'
 import type { EventFiltered } from '@/services/events/Types'
+import Link from 'next/link'
+import Image from '@/components/Image/Image'
 
 type PropTypes = {
     event: EventFiltered & {
@@ -14,12 +15,19 @@ type PropTypes = {
 
 export default function EventCard({ event }: PropTypes) {
     return (
-        <ImageCard 
+        <Link
             className={styles.EventCard}
             title={event.name} 
             href={`/events/${event.order}/${event.name}`} 
-            image={event.coverImage.image}
         >
+            <div className={styles.coverImage}>
+            {
+                event.coverImage.image ? <Image 
+                    image={event.coverImage.image} 
+                    width={300} 
+                /> : <></>
+            }
+            </div>
             <ul>
                 <li>
                     <FontAwesomeIcon icon={faCalendar} />
@@ -40,6 +48,6 @@ export default function EventCard({ event }: PropTypes) {
                     ) : <></>
                 }
             </ul>
-        </ImageCard>
+        </Link>
     )
 }
