6acd61c8c4c0a4068c67da9b1f5c5f324fede2b7
ablerto <albertroed@hotmail.com>
2024-09-26 21:15:51 +0200

    chore: adds cakes route

---
+++ src/app/cakes/page.tsx
@@ -0,0 +1,10 @@
+import PageWrapper from '../_components/PageWrapper/PageWrapper';
+import styles from './page.module.scss'
+export default async function cakes() {
+    return (
+        <PageWrapper title='Kaker'>
+            
+            Kake er godt
+        </PageWrapper>
+    );
+}
