8dd696c0b68b28779d38abc1d243f2a44c1ac801
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-10-01 13:36:33 +0200

    chore: changes createImage action to create 3 images

--- src/services/images/ConfigVars.ts
+++ src/services/images/ConfigVars.ts
@@ -1,3 +1,9 @@
 export const maxFileSize = 100 * 1024 * 1024 // 100mb
 
 export const maxNumberOfImagesInOneBatch = 10
+
+export const imageSizes = {
+    small: 180,
+    medium: 360,
+    large: 720,
+}
