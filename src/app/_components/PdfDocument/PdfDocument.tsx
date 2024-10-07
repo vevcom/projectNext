95bd45bc83dd777b6852276b910c5efb177d1b11
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-10-07 16:17:25 +0200

    fix: changes pdf-worker to make it build in prod

--- src/app/_components/PdfDocument/PdfDocument.tsx
+++ src/app/_components/PdfDocument/PdfDocument.tsx
@@ -12,10 +12,7 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 import 'react-pdf/dist/esm/Page/TextLayer.css'
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-  ).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
 
 type PropTypes = {
     src: string
@@ -95,6 +92,17 @@ export default function PdfDocument({ src, className }: PropTypes) {
         if (leftPage) return `Side ${leftPage} av ${numPages}`
         if (rightPage) return `Side ${rightPage} av ${numPages}`
         return ''
+    }
+
+    if (typeof window !== 'undefined' && !window.Promise.withResolvers) {
+        return (
+            <>
+            <p className={styles.fallbackWarning}>
+                Din nettleser støtter ikke vevens fantastiske PDF-løsning. Bruker alternativ løsning.
+            </p>
+            <embed className={styles.fallback} src={src} type="application/pdf" />
+            </>
+        )
     }
 
     return (
