e6302b9db620fb57646e49bd975b82d601ba5c92
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-10-09 19:40:33 +0200

    chore: makes sure all profile images are migrated into profileimage collection

--- src/prisma/prismaservice/src/dobbelOmega/dobbelOmega.ts
+++ src/prisma/prismaservice/src/dobbelOmega/dobbelOmega.ts
@@ -24,7 +24,7 @@ export default async function dobbelOmega(pnPrisma: PrismaClientPn) {
 
     const imageCollectionIdMap = await migrateImageCollections(pnPrisma, vevenPrisma)
     const imageIdMap = await migrateImages(pnPrisma, vevenPrisma, imageCollectionIdMap, limits)
-    await migrateUsers(pnPrisma, vevenPrisma, limits)
+    await migrateUsers(pnPrisma, vevenPrisma, limits, imageIdMap)
     await migrateOmbul(pnPrisma, vevenPrisma, imageIdMap, limits)
     await migrateOmegaquotes(pnPrisma, vevenPrisma, limits)
     await migrateArticles(pnPrisma, vevenPrisma, imageIdMap, limits)
