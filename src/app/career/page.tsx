6ea4fc699ba65ae0bb243dc27bc13460ac7202a8
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-10-18 22:18:04 +0200

    feat: adds a description about contactor (cms paragraph) to the career landing page

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,12 +1,13 @@
-import { Session } from "@/auth/Session";
-import PageWrapper from "../_components/PageWrapper/PageWrapper";
+import { Session } from '@/auth/Session'
+import PageWrapper from '@/components/PageWrapper/PageWrapper'
+import SpecialCmsParagraph from '@/components/Cms/CmsParagraph/SpecialCmsParagraph'
 
 export default async function CareerLandingPage() {
     const session = await Session.fromNextAuth()
 
     return (
-        <PageWrapper title={session.user ? "Karriere" : "For bedrifter"}>
-            <></>
+        <PageWrapper title={session.user ? 'Karriere' : 'For bedrifter'}>
+            <SpecialCmsParagraph special="CAREER_INFO" />
         </PageWrapper>
     )
 }
