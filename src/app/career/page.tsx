0b3679a29594a9dd2f49dfa07719aa55345b7c0f
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-10-18 23:19:18 +0200

    chore: display cms link to contactor on career page

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -5,18 +5,27 @@ import styles from './page.module.scss'
 import Link from 'next/link'
 import Image from '@/components/Image/Image'
 import { readSpecialImageAction } from '@/actions/images/read'
+import { readSpecialCmsLinkAction } from '@/actions/cms/links/read'
+import CmsLink from '../_components/Cms/CmsLink/CmsLink'
 
 export default async function CareerLandingPage() {
     const session = await Session.fromNextAuth()
     const jobAdImageRes = await readSpecialImageAction('ENGINEER')
     const eventImageRes = await readSpecialImageAction('FAIR')
     const comanyImageRes = await readSpecialImageAction('SKYSCRAPER')
+    const conactorCmsLinkRes = await readSpecialCmsLinkAction.bind(
+        null, { special: 'CAREER_LINK_TO_CONTACTOR' }
+    )()
     const jobAdImage = jobAdImageRes.success ? jobAdImageRes.data : null
     const eventImage = eventImageRes.success ? eventImageRes.data : null
     const companyImage = comanyImageRes.success ? comanyImageRes.data : null
+    const contactorCmsLink = conactorCmsLinkRes.success ? conactorCmsLinkRes.data : null
 
     return (
-        <PageWrapper title={session.user ? 'Karriere' : 'For bedrifter'}>
+        <PageWrapper title={session.user ? 'Karriere' : 'For bedrifter'} headerItem={ 
+            contactorCmsLink ? <CmsLink 
+            className={styles.conactorLink} cmsLink={contactorCmsLink} /> : <></> 
+        }>
             <div className={styles.wrapper}>
                 <SpecialCmsParagraph className={styles.info} special="CAREER_INFO" />
                 <span className={styles.links}>
