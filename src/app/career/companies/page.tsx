be86db903e41d1273cab28d714b260ac41ce7d00
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-10-20 19:27:04 +0200

    feat: makes form to create a company

--- src/app/career/companies/page.tsx
+++ src/app/career/companies/page.tsx
@@ -1,9 +1,23 @@
+import { createCompanyAction } from '@/actions/career/companies/create';
+import Form from '@/app/_components/Form/Form';
+import { AddHeaderItemPopUp } from '@/app/_components/HeaderItems/HeaderItemPopUp';
+import TextInput from '@/app/_components/UI/TextInput';
 import PageWrapper from '@/components/PageWrapper/PageWrapper';
 
 export default function page() {
     return (
         <PageWrapper title="Bedrifter" headerItem={
-            <></>
+            <AddHeaderItemPopUp PopUpKey="CreateCompany">
+                <Form
+                    title="Ny bedrift"
+                    action={createCompanyAction.bind(null, {})}
+                    refreshOnSuccess
+                    closePopUpOnSuccess="CreateCompany"
+                >
+                    <TextInput name="name" label="Navn" />
+                    <TextInput name="description" label="Beskrivelse" />
+                </Form>
+            </AddHeaderItemPopUp>
         }>
             <></>
         </PageWrapper>
