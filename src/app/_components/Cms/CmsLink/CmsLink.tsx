c066ac56b8b4b10c23bff0dff7cb47f087bac867
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2024-11-07 19:17:05 +0100

    feat: add symbol and target _blank for external cms links

--- src/app/_components/Cms/CmsLink/CmsLink.tsx
+++ src/app/_components/Cms/CmsLink/CmsLink.tsx
@@ -2,6 +2,8 @@ import styles from './CmsLink.module.scss'
 import CmsLinkEditor from './CmsLinkEditor'
 import Link from 'next/link'
 import type { CmsLinkInfered } from '@/services/cms/links/Types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLink } from '@fortawesome/free-solid-svg-icons'
 
 type PropTypes = {
     cmsLink: CmsLinkInfered
@@ -10,9 +12,16 @@ type PropTypes = {
 }
 
 export default function CmsLink({ cmsLink, className, color = 'secondary' }: PropTypes) {
+    const isExternal = cmsLink.rawUrl && cmsLink.rawUrl.startsWith('http')
+
     return (
         <div className={`${styles.CmsLink} ${className}`}>
-            <Link href={cmsLink.url} className={`${styles.CmsLink} ${styles[color]}`}>
+            <Link href={cmsLink.url} className={`${styles.CmsLink} ${styles[color]}`} target={isExternal ? "_blank" : undefined}>
+                { 
+                    isExternal && (
+                        <FontAwesomeIcon className={styles.extenalLinkIcon} icon={faExternalLink} />
+                    ) 
+                }
                 {cmsLink.text}
             </Link>
             <CmsLinkEditor cmsLink={cmsLink} />
