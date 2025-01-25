0c6222ce0720166f044eb759836d61fdaf33a757
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2025-01-25 20:08:09 +0100

    feat: Add new Radio UI component

---
+++ src/app/_components/UI/RadioLarge.tsx
@@ -0,0 +1,40 @@
+import styles from './RadioLarge.module.scss'
+import { v4 as uuid } from 'uuid'
+
+
+export default function RadioLarge<ValueType extends string>({
+    name,
+    options,
+    defaultValue,
+    onChange,
+}: {
+    name: string,
+    defaultValue?: ValueType,
+    onChange?: (newValue: ValueType) => unknown,
+    options: {
+        value: ValueType,
+        label: string,
+    }[]
+}) {
+    return <div className={styles.radioContainer}>
+        {options.map(option => {
+            const id = uuid()
+            return <div key={uuid()}>
+                <input
+                    type="radio"
+                    name={name}
+                    id={id}
+                    defaultChecked={defaultValue ? defaultValue === option.value : false}
+                    onChange={() => {
+                        if (onChange) {
+                            onChange(option.value)
+                        }
+                    }}
+                />
+                <label htmlFor={id} >
+                    {option.label}
+                </label>
+            </div>
+        })}
+    </div>
+}
