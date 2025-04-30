32e5246e2262f24ea4b5bf3cc76bda3186ddcee5
Paulius Juzenas <pauliusj@stud.ntnu.no>
2025-05-01 00:03:02 +0200

    refactor: stripe event handling

--- src/app/api/stripe-event/route.ts
+++ src/app/api/stripe-event/route.ts
@@ -1,6 +1,6 @@
 import logger from '@/lib/logger'
 import { stripe } from '@/lib/stripe'
-import prisma from '@/prisma'
+import { DepositMethods } from '@/services/ledger/transactions/deposits/methods'
 
 export async function POST(req: Request) {
     if (!process.env.STRIPE_WEBHOOK_SECRET) {
@@ -15,33 +15,29 @@ export async function POST(req: Request) {
     }
 
     const event = stripe.webhooks.constructEvent(body, stripeSignature, process.env.STRIPE_WEBHOOK_SECRET)
-
+    
+    // Check if the event is one of the expected types
     if (event.type !== 'charge.succeeded' && event.type !== 'charge.updated') {
         logger.warn(`Unhandled Stripe event received: ${event.type}`)
         return new Response('', { status: 200 })
     }
 
+    // Validate the event data types we need
     if (typeof event.data.object.balance_transaction !== 'string' || typeof event.data.object.payment_intent !== 'string') {
         return new Response('', { status: 200 })
     }
 
-    const balanceTransaction = await stripe.balanceTransactions.retrieve(event.data.object.balance_transaction)
-
-    const { transactionId } = await prisma.stripeDeposit.findUniqueOrThrow({
-        where: {
-            paymentIntentId: event.data.object.payment_intent,
-        }
-    })
-
-    await prisma.transaction.update({
-        where: {
-            id: transactionId,
-        },
-        data: {
-            status: 'SUCCEEDED',
-            fee: balanceTransaction.fee,
-        }
-    })
+    try {
+        await DepositMethods.confirmStripe.newClient().execute({ 
+            session: null, 
+            params: {
+                balanceTransactionId: event.data.object.balance_transaction,
+                paymentIntentId: event.data.object.payment_intent,
+            },
+        })
+    } catch {
+        return new Response('Server-side error confirming deposit', { status: 500 })
+    }
 
     return new Response('', { status: 200 })
 }
