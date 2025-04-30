e7cb2b1898fed082c2a400a17655ec99ce513a1f
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2025-04-30 20:35:37 +0200

    chore(seeder): Add a image to the seeded jobads

--- src/prisma/seeder/src/development/seedDevJobAds.ts
+++ src/prisma/seeder/src/development/seedDevJobAds.ts
@@ -1,68 +1,80 @@
 import { JobadMethods } from '@/services/career/jobAds/methods'
-import type { PrismaClient } from '@prisma/client'
+import type { JobType, Prisma, PrismaClient } from '@prisma/client'
 
 
 export default async function seedDevJobAds(prisma: PrismaClient) {
     const tomorrow = new Date()
     tomorrow.setDate(tomorrow.getDate() + 1)
 
-    JobadMethods.create.client(prisma).execute({
-        data: {
+    const image = await prisma.image.findUniqueOrThrow({
+        where: {
+            special: 'FAIR',
+        }
+    })
+
+    const jobAdData: {
+        type: JobType,
+        applicationDeadline: Date,
+        articleName: string,
+        description: string,
+    }[] = [
+        {
             type: 'FULL_TIME',
             applicationDeadline: tomorrow,
             articleName: 'Vever',
             description: 'Vevcom søker nye vevere for å febrisk kode ferdig projectNext',
-            companyId: 1,
         },
-        bypassAuth: true,
-        session: null,
-    })
-
-    JobadMethods.create.client(prisma).execute({
-        data: {
+        {
             type: 'PART_TIME',
             applicationDeadline: tomorrow,
             articleName: 'Medlem i Hovedstyret',
             description: 'Er du flink til å gå? Da er du perfetk i rollen i HS',
-            companyId: 1,
         },
-        bypassAuth: true,
-        session: null,
-    })
-
-    JobadMethods.create.client(prisma).execute({
-        data: {
+        {
             type: 'INTERNSHIP',
             applicationDeadline: tomorrow,
             articleName: 'Locmeister',
             description: 'Elsker du lokomotiv? Da er Locmeiser din drømme sommerjobb!',
-            companyId: 1,
         },
-        bypassAuth: true,
-        session: null,
-    })
-
-    JobadMethods.create.client(prisma).execute({
-        data: {
+        {
             type: 'CONTRACT',
             applicationDeadline: tomorrow,
             articleName: 'Konsulent',
             description: 'Elsker du penger? Da er dette drømmen jobben for deg, her tjener du massevis av penger.',
-            companyId: 1,
         },
-        bypassAuth: true,
-        session: null,
-    })
-
-    JobadMethods.create.client(prisma).execute({
-        data: {
+        {
             type: 'OTHER',
             applicationDeadline: tomorrow,
             articleName: 'Spåmann',
             description: 'Føler du at du kan se inn i fremtiden? Da vet du allerede om du får jobben eller ikke.',
-            companyId: 1,
         },
-        bypassAuth: true,
-        session: null,
-    })
+    ]
+
+    for (const jobAd of jobAdData) {
+        const restult = await JobadMethods.create.client(prisma).execute({
+            data: {
+                ...jobAd,
+                companyId: 1,
+            },
+            bypassAuth: true,
+            session: null,
+        })
+
+        await prisma.article.update({
+            where: {
+                id: restult.articleId
+            },
+            data: {
+                coverImage: {
+                    update: {
+                        image: {
+                            connect: {
+                                id: image.id
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    }
 }
