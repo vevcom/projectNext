68db883474de56174b66cb60b9941212c322656a
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2025-05-01 23:54:30 +0200

    teat: Always show the test coverage when running the tests

--- jest.config.ts
+++ jest.config.ts
@@ -7,12 +7,15 @@ const config: Config = {
     coverageProvider: 'v8',
     testEnvironment: './tests/PrismaTestEnvironment.ts',
     setupFilesAfterEnv: ['./tests/setup.ts'],
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+    coverageReporters: ['text-summary'],
 }
 
 export default async function jestConfig() {
     const base = await createJestConfig(config)()
     return {
         ...base,
-        transformIgnorePatterns: [],
+        transformIgnorePatterns: [], // This is due to a wierd bug
     }
 }
