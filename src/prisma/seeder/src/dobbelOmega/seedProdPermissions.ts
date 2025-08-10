da296e9db56a07c97f2de46a1b2b01ef579c26ab
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2025-08-10 15:48:26 +0200

    feat(dobbel-omega): Add seeding of prod permissions

--- src/prisma/seeder/src/dobbelOmega/seedProdPermissions.ts
+++ src/prisma/seeder/src/dobbelOmega/seedProdPermissions.ts
@@ -1,40 +1,124 @@
-import type { Permission, PrismaClient as PrismaClientPn } from '@prisma/client'
+import { Permission } from '@prisma/client'
+import type { OmegaMembershipLevel, PrismaClient as PrismaClientPn } from '@prisma/client'
 
+function checkForPuplicates(arr: Permission[], failMessage: string) {
+    const permissionSet = new Set(arr)
+    if (permissionSet.size !== arr.length) {
+        const duplicates = arr.filter((perm, index) => arr.indexOf(perm) !== index)
+        throw new Error(
+            `A duplicate permission is trying to be added to ${failMessage}, duplicates: ${duplicates.join(', ')}`
+        )
+    }
+}
 
 export default async function seedProdPermissions(prisma: PrismaClientPn) {
     const defaultPermissions: Permission[] = [
-        'OMEGAQUOTES_WRITE',
-        'OMEGAQUOTES_READ',
-        'OMBUL_READ',
         'GROUP_READ',
         'CLASS_READ',
-        'COMMITTEE_READ',
-        'INTEREST_GROUP_READ',
-        'STUDY_PROGRAMME_READ',
         'JOBAD_READ',
-        'LOCKER_USE',
-        'USERS_READ',
         'SCHOOLS_READ',
         'COURSES_READ',
-        'EVENT_READ',
-        'EVENT_REGISTRATION_READ',
-        'EVENT_REGISTRATION_CREATE',
-        'PURCHASE_CREATE',
         'CABIN_CALENDAR_READ',
         'CABIN_BOOKING_CABIN_CREATE',
         'CABIN_BOOKING_BED_CREATE'
     ]
 
-    const permissionSet = new Set(defaultPermissions)
-    if (permissionSet.size !== defaultPermissions.length) {
-        const duplicates = defaultPermissions.filter((perm, index) => defaultPermissions.indexOf(perm) !== index)
-        throw new Error(`A duplicate permission is trying to be added: ${duplicates.join(', ')}`)
-    }
+    checkForPuplicates(defaultPermissions, 'default permissions')
 
     await prisma.defaultPermission.createMany({
         data: defaultPermissions.map(perm => ({
             permission: perm
         }))
     })
+
+    const membershipPermissions: Record<OmegaMembershipLevel, Permission[]> = {
+        MEMBER: [
+            'OMBUL_READ',
+            'OMEGAQUOTES_READ',
+            'OMEGAQUOTES_WRITE',
+            'COMMITTEE_READ',
+            'INTEREST_GROUP_READ',
+            'STUDY_PROGRAMME_READ',
+            'LOCKER_USE',
+            'EVENT_READ',
+            'EVENT_REGISTRATION_READ',
+            'EVENT_REGISTRATION_CREATE',
+            'PURCHASE_CREATE',
+            'USERS_READ',
+        ],
+        SOELLE: [
+            'OMBUL_READ',
+            'OMEGAQUOTES_READ',
+            'COMMITTEE_READ',
+            'INTEREST_GROUP_READ',
+            'STUDY_PROGRAMME_READ',
+            'EVENT_READ',
+            'EVENT_REGISTRATION_READ',
+            'USERS_READ',
+        ],
+        EXTERNAL: []
+    }
+
+    checkForPuplicates(membershipPermissions.MEMBER, 'MEMBER permissions')
+    checkForPuplicates(membershipPermissions.SOELLE, 'SOELLE permissions')
+    checkForPuplicates(membershipPermissions.EXTERNAL, 'EXTERNAL permissions')
+
+    for (const [level, permissions] of Object.entries(membershipPermissions)) {
+        const group = await prisma.omegaMembershipGroup.findUniqueOrThrow({
+            where: {
+                omegaMembershipLevel: level as OmegaMembershipLevel,
+            }
+        })
+
+        await prisma.groupPermission.createMany({
+            data: permissions.map(perm => ({
+                permission: perm,
+                groupId: group.id
+            }))
+        })
+    }
+
+    const allPermissions = Object.values(Permission)
+
+    const committeePermissions: Record<string, Permission[]> = {
+        vevcom: allPermissions,
+        hs: allPermissions,
+        ombul: [
+            'OMBUL_READ',
+            'OMBUL_CREATE',
+            'OMBUL_UPDATE',
+            'OMBUL_DESTROY',
+        ],
+        hyttecom: [
+            'CABIN_ADMIN',
+            'CABIN_BOOKING_ADMIN',
+            'CABIN_PRODUCTS_ADMIN',
+        ],
+        contactor: [
+            'DOTS_ADMIN',
+        ],
+    }
+
+    for (const [shortName, permissions] of Object.entries(committeePermissions)) {
+        checkForPuplicates(permissions, `${shortName} permissions`)
+
+        const committee = await prisma.committee.findUnique({
+            where: {
+                shortName,
+            }
+        })
+        if (!committee) {
+            console.warn(`Committee with shortName ${shortName} not found, skipping permissions creation.
+This should never happen with no limits`)
+            continue
+        }
+
+        await prisma.groupPermission.createMany({
+            data: permissions.map(perm => ({
+                permission: perm,
+                groupId: committee.groupId
+            }))
+        })
+    }
 }
 
