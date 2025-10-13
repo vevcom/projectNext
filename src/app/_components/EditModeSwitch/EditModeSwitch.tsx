ffbef5d508a15d4e5824fcf43b6d287ebae0a301
Vilde Viksøy <vildeviksoy@gmail.com>
2025-10-13 21:18:02 +0200

    fix linting errors

--- src/app/_components/EditModeSwitch/EditModeSwitch.tsx
+++ src/app/_components/EditModeSwitch/EditModeSwitch.tsx
@@ -1,28 +1,28 @@
 'use client'
 import styles from './EditModeSwitch.module.scss'
 import { EditModeContext } from '@/contexts/EditMode'
+import useKeyPress from '@/hooks/useKeyPress'
 import { useContext, useEffect, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil } from '@fortawesome/free-solid-svg-icons'
 import type { ChangeEvent } from 'react'
-import useKeyPress from '@/hooks/useKeyPress'
 
 export default function EditModeSwitch() {
     const editingContext = useContext(EditModeContext)
     if (!editingContext) throw new Error('No EditModeContext found')
-        
+
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         editingContext.setEditMode(e.target.checked)
     }
-                
+
     const ref = useRef<HTMLInputElement>(null)
-            
-    useKeyPress('ø', (event:KeyboardEvent) => {  
+
+    useKeyPress('ø', (event:KeyboardEvent) => {
         if (event.ctrlKey) {
             editingContext.setEditMode(!editingContext.editMode)
         }
     })
-    
+
     useEffect(() => {
         if (ref.current?.checked) {
             ref.current.checked = editingContext.editMode
@@ -30,7 +30,6 @@ export default function EditModeSwitch() {
     })
     if (!editingContext.somethingToEdit) return null
 
-    
 
     return (
         <div className={styles.EditModeSwitch}>
