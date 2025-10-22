ff4c2335acf867f124b23258e01e98df95d4b26d
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2025-10-22 20:20:13 +0200

    refactor: make all article actions into (sub)operations

--- src/services/cms/articles/operations.ts
+++ src/services/cms/articles/operations.ts
@@ -1,12 +1,15 @@
 import '@pn-server-only'
-import { articleRealtionsIncluder } from './constants'
+import { articleRealtionsIncluder, maxSections } from './constants'
 import { articleSchemas } from './schemas'
 import { defineOperation, defineSubOperation } from '@/services/serviceOperation'
+import { articleSectionOperations } from '@/cms/articleSections/operations'
 import { ServerOnly } from '@/auth/auther/ServerOnly'
 import logger from '@/lib/logger'
+import { ServerError } from '@/services/error'
 import { SpecialCmsArticle } from '@prisma/client'
 import { z } from 'zod'
 import { v4 } from 'uuid'
+import type { ArticleSectionPart } from '@/cms/articleSections/types'
 
 const create = defineOperation({
     authorizer: ServerOnly,
@@ -37,9 +40,22 @@ const create = defineOperation({
     }
 })
 
+const destroy = defineOperation({
+    authorizer: ServerOnly,
+    paramsSchema: z.object({
+        id: z.number()
+    }),
+    operation: ({ prisma, params }) =>
+        prisma.article.delete({
+            where: {
+                id: params.id
+            }
+        })
+})
+
 export const articleOperations = {
     create,
-
+    destroy,
     readSpecial: defineSubOperation({
         paramsSchema: () => z.object({
             special: z.nativeEnum(SpecialCmsArticle),
@@ -59,6 +75,157 @@ export const articleOperations = {
                 ...article,
                 coverImage: article.coverImage
             }
+        }
+    }),
+    /**
+     * Update the article metadata like name
+     */
+    update: defineSubOperation({
+        paramsSchema: () => z.object({
+            articleId: z.number(),
+        }),
+        dataSchema: () => articleSchemas.update,
+        operation: () => async ({ prisma, params, data }) =>
+            prisma.article.update({
+                where: { id: params.articleId },
+                data,
+                include: articleRealtionsIncluder,
+            })
+    }),
+    addSection: defineSubOperation({
+        paramsSchema: () => z.object({
+            articleId: z.number(),
+        }),
+        dataSchema: () => articleSchemas.addSection,
+        operation: () => async ({ prisma, params, data }) => {
+            const article = await prisma.article.findUnique({
+                where: {
+                    id: params.articleId,
+                },
+            })
+            if (!article) throw new ServerError('NOT FOUND', 'Artikkel ikke funnet.')
+
+            const highestOrderSection = await prisma.articleSection.findMany({
+                where: {
+                    articleId: params.articleId,
+                },
+                orderBy: {
+                    order: 'desc',
+                },
+                take: 1,
+            })
+            // Get the order of the highest order section, or 0 if there are no sections
+            const nextOrder = highestOrderSection.length > 0 ? highestOrderSection[0].order + 1 : 0
+
+            const numberOfSections = await prisma.articleSection.count({
+                where: {
+                    articleId: params.articleId,
+                },
+            })
+            if (numberOfSections >= maxSections) {
+                throw new ServerError('BAD PARAMETERS', `The maximum number of sections is ${maxSections}`)
+            }
+
+            const updatedArticle = await prisma.article.update({
+                where: {
+                    id: params.articleId,
+                },
+                data: {
+                    articleSections: {
+                        create: {
+                            order: nextOrder,
+                        },
+                    },
+                },
+                include: articleRealtionsIncluder,
+            })
+
+            const addedArticleSectionId = updatedArticle.articleSections[updatedArticle.articleSections.length - 1].id
+
+            for (const part of ['cmsParagraph', 'cmsLink', 'cmsImage'] satisfies ArticleSectionPart[]) {
+                if (data.includeParts[part]) {
+                    await articleSectionOperations.addPart.internalCall({
+                        data: {
+                            part,
+                        },
+                        params: {
+                            id: addedArticleSectionId
+                        }
+                    })
+                }
+            }
+            return updatedArticle
+        }
+    }),
+    reorderSections: defineSubOperation({
+        paramsSchema: () => z.object({
+            articleId: z.number(),
+            sectionId: z.number(),
+        }),
+        opensTransaction: true,
+        dataSchema: () => articleSchemas.reorderSections,
+        operation: () => async ({ prisma, params, data }) => {
+            const section = await prisma.articleSection.findUnique({
+                where: {
+                    articleId: params.articleId,
+                    id: params.sectionId,
+                },
+            })
+            if (!section) throw new ServerError('NOT FOUND', 'Seksjon ikke funnet.')
+
+            //find the section with the order one higher/lower than the current section
+            const otherSection = await prisma.articleSection.findMany({
+                where: {
+                    articleId: params.articleId,
+                    order: data.direction === 'UP' ? {
+                        lt: section.order,
+                    } : {
+                        gt: section.order,
+                    },
+                },
+                orderBy: {
+                    order: data.direction === 'UP' ? 'desc' : 'asc',
+                },
+                take: 1,
+            }).then(
+                res => (res.length > 0 ? res[0] : null)
+            )
+            if (!otherSection) throw new ServerError('BAD PARAMETERS', 'Seksjon kan ikke flyttes opp/ned.')
+
+            //flip thir order numbers
+            const tempOrder = -1 // Or any other value that won't violate the unique constraint
+
+            // First, set the order of the section to the temporary value
+            return await prisma.$transaction(async (tx) => {
+                await tx.articleSection.update({
+                    where: {
+                        articleId: params.articleId,
+                        id: section.id
+                    },
+                    data: { order: tempOrder },
+                })
+                const updatedOtherSection = await tx.articleSection.update({
+                    where: {
+                        articleId: params.articleId,
+                        id: otherSection.id
+                    },
+                    data: { order: section.order },
+                })
+                // Finally, set the order of the section to the otherSection's original order
+                const updatedSection = prisma.articleSection.update({
+                    where: {
+                        articleId: params.articleId,
+                        id: section.id
+                    },
+                    data: { order: otherSection.order },
+                })
+
+                if (!updatedSection || !updatedOtherSection) {
+                    throw new ServerError('UNKNOWN ERROR', 'Noe uventet skjedde under flytting av seksjonen.')
+                }
+
+                return updatedSection
+            })
         }
     })
 }
--- src/services/cms/articles/schemas.ts
+++ src/services/cms/articles/schemas.ts
@@ -1,17 +1,26 @@
+import { baseSchema as baseSchemaArticleSections } from '@/cms/articleSections/schemas'
 import { SpecialCmsArticle } from '@prisma/client'
 import { z } from 'zod'
 
-const baseSchemas = z.object({
+const baseSchema = z.object({
     name: z.string().min(2, 'Minimum lengde er 2 tegn.').max(20, 'Maksimum lengde er 20 tegn.'),
-    special: z.nativeEnum(SpecialCmsArticle).optional()
+    special: z.nativeEnum(SpecialCmsArticle).optional(),
+    includeParts: z.record(baseSchemaArticleSections.shape.part, z.boolean()),
+    direction: z.union([z.literal('UP'), z.literal('DOWN')])
 })
 
 export const articleSchemas = {
-    create: baseSchemas.pick({
+    create: baseSchema.pick({
         name: true,
         special: true
     }).partial(),
-    update: baseSchemas.pick({
+    update: baseSchema.pick({
         name: true,
-    }).partial()
+    }).partial(),
+    addSection: baseSchema.pick({
+        includeParts: true
+    }),
+    reorderSections: baseSchema.pick({
+        direction: true
+    }),
 } as const
