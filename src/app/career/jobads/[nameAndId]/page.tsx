b5106f270524302478b410b2669c15d9cddfef89
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2025-10-23 16:11:52 +0200

    chore: use the actions in jobads

--- src/app/career/jobads/[nameAndId]/page.tsx
+++ src/app/career/jobads/[nameAndId]/page.tsx
@@ -6,7 +6,19 @@ import { CompanyPagingProvider } from '@/contexts/paging/CompanyPaging'
 import Company from '@/components/Company/Company'
 import Date from '@/components/Date/Date'
 import { Session } from '@/auth/session/Session'
-import { readJobAdAction } from '@/services/career/jobAds/actions'
+import {
+    readJobAdAction,
+    updateJobAdArticleAction,
+    updateJobAdArticleAddSectionAction,
+    updateJobAdArticleCmsImageAction,
+    updateJobAdArticleCmsLinkAction,
+    updateJobAdArticleCmsParagraphAction,
+    updateJobAdArticleCoverImageAction,
+    updateJobAdArticleReorderSectionsAction,
+    updateJobAdArticleSectionAction,
+    updateJobAdArticleSectionsAddPartAction,
+    updateJobAdArticleSectionsRemovePartAction
+} from '@/services/career/jobAds/actions'
 import { jobAdType } from '@/services/career/jobAds/constants'
 import { decodeVevenUriHandleError } from '@/lib/urlEncoding'
 import { notFound } from 'next/navigation'
@@ -19,6 +31,7 @@ import {
     faSuitcase,
     faXmarkCircle
 } from '@fortawesome/free-solid-svg-icons'
+import { configureAction } from '@/services/configureAction'
 
 type PropTypes = {
     params: Promise<{
@@ -40,54 +53,101 @@ export default async function JobAd({ params }: PropTypes) {
     return (
         <div className={styles.wrapper}>
             <main>
-                <Article article={jobAd.article} sideBarClassName={styles.sideBar} sideBarContent={
-                    <>
-                        <ul className={styles.metInfo}>
-                            <li>
-                                <FontAwesomeIcon icon={faSuitcase} />
-                                <h3>Stillingstype</h3>
-                                <p>{jobAdType[jobAd.type].label}</p>
-                            </li>
-                            <li>
-                                <FontAwesomeIcon icon={jobAd.active ? faCheckCircle : faXmarkCircle} />
-                                <h3>Status</h3>
-                                <p>{jobAd.active ? 'Denne jobbanonsen er aktiv' : 'Denne jobbanonsen er arkivert'}</p>
-                            </li>
-                            <li>
-                                <FontAwesomeIcon icon={faClock} />
-                                <h3>Søknadsfrist</h3>
-                                <p>
-                                    { jobAd.applicationDeadline ?
-                                        <Date date={jobAd.applicationDeadline} /> :
-                                        'Ingen søknadsfrist satt'
-                                    }
-                                </p>
-                            </li>
-                            <li>
-                                <FontAwesomeIcon icon={faNewspaper} />
-                                <h3>Publisert</h3>
-                                <p>
-                                    <Date date={jobAd.createdAt} />
-                                </p>
-                            </li>
-                            <li>
-                                <FontAwesomeIcon icon={faLocationDot} />
-                                <h3>Sted</h3>
-                                <p>{jobAd.location}</p>
-                            </li>
-                        </ul>
-                        <div className={styles.company}>
-                            <h2>Arbeidsgiver</h2>
-                            <Company
-                                disableEdit
-                                squareLogo={false}
-                                company={jobAd.company}
-                                asClient={false}
-                                session={session}
-                            />
-                        </div>
-                    </>
-                } />
+                <Article
+                    article={jobAd.article}
+                    sideBarClassName={styles.sideBar}
+                    actions={{
+                        updateArticleAction: configureAction(
+                            updateJobAdArticleAction,
+                            { implementationParams: { jobAdId: jobAd.id } }
+                        ),
+                        updateCoverImageAction: configureAction(
+                            updateJobAdArticleCoverImageAction,
+                            { implementationParams: { jobAdId: jobAd.id } }
+                        ),
+                        addSectionToArticleAction: configureAction(
+                            updateJobAdArticleAddSectionAction,
+                            { implementationParams: { jobAdId: jobAd.id } }
+                        ),
+                        reorderArticleSectionsAction: configureAction(
+                            updateJobAdArticleReorderSectionsAction,
+                            { implementationParams: { jobAdId: jobAd.id } }
+                        ),
+                        articleSections: {
+                            updateCmsParagraph: configureAction(
+                                updateJobAdArticleCmsParagraphAction,
+                                { implementationParams: { jobAdId: jobAd.id } }
+                            ),
+                            updateCmsImage: configureAction(
+                                updateJobAdArticleCmsImageAction,
+                                { implementationParams: { jobAdId: jobAd.id } }
+                            ),
+                            updateCmsLink: configureAction(
+                                updateJobAdArticleCmsLinkAction,
+                                { implementationParams: { jobAdId: jobAd.id } }
+                            ),
+                            updateArticleSection: configureAction(
+                                updateJobAdArticleSectionAction,
+                                { implementationParams: { jobAdId: jobAd.id } }
+                            ),
+                            addPartToArticleSection: configureAction(
+                                updateJobAdArticleSectionsAddPartAction,
+                                { implementationParams: { jobAdId: jobAd.id } }
+                            ),
+                            removePartFromArticleSection: configureAction(
+                                updateJobAdArticleSectionsRemovePartAction,
+                                { implementationParams: { jobAdId: jobAd.id } }
+                            )
+                        }
+                    }}
+                    sideBarContent={
+                        <>
+                            <ul className={styles.metInfo}>
+                                <li>
+                                    <FontAwesomeIcon icon={faSuitcase} />
+                                    <h3>Stillingstype</h3>
+                                    <p>{jobAdType[jobAd.type].label}</p>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon icon={jobAd.active ? faCheckCircle : faXmarkCircle} />
+                                    <h3>Status</h3>
+                                    <p>{jobAd.active ? 'Denne jobbanonsen er aktiv' : 'Denne jobbanonsen er arkivert'}</p>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon icon={faClock} />
+                                    <h3>Søknadsfrist</h3>
+                                    <p>
+                                        { jobAd.applicationDeadline ?
+                                            <Date date={jobAd.applicationDeadline} /> :
+                                            'Ingen søknadsfrist satt'
+                                        }
+                                    </p>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon icon={faNewspaper} />
+                                    <h3>Publisert</h3>
+                                    <p>
+                                        <Date date={jobAd.createdAt} />
+                                    </p>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon icon={faLocationDot} />
+                                    <h3>Sted</h3>
+                                    <p>{jobAd.location}</p>
+                                </li>
+                            </ul>
+                            <div className={styles.company}>
+                                <h2>Arbeidsgiver</h2>
+                                <Company
+                                    disableEdit
+                                    squareLogo={false}
+                                    company={jobAd.company}
+                                    asClient={false}
+                                    session={session}
+                                />
+                            </div>
+                        </>
+                    } />
             </main>
             <CompanyPagingProvider
                 serverRenderedData={[]}
