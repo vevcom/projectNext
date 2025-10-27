4037a0ed9d4216f07c5b1243da9093b0717086c5
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2025-10-27 13:33:34 +0100

    chore: set up visibility auther

--- src/auth/auther/RequireVisibility.ts
+++ src/auth/auther/RequireVisibility.ts
@@ -1,14 +1,14 @@
 import { AutherFactory } from './Auther'
-import { checkVisibility } from '@/auth/checkVisibility'
-import type { VisibilityCollapsed } from '@/services/visibility/types'
+import { checkVisibility } from '@/auth/visibility/checkVisibility'
+import type { VisibilityMatrix } from '@/services/visibility/types'
 import type { Permission } from '@prisma/client'
 
 export const RequireVisibility = AutherFactory<
     { bypassPermission: Permission },
-    { visibility: VisibilityCollapsed },
+    { visibility: VisibilityMatrix },
     'USER_NOT_REQUIERED_FOR_AUTHORIZED'
 > (({ session, dynamicFields, staticFields }) => ({
-    success: checkVisibility(session, dynamicFields.visibility, 'REGULAR') ||
+    success: checkVisibility(session.memberships, dynamicFields.visibility) ||
         session.permissions.includes(staticFields.bypassPermission),
     session,
 }))
--- src/auth/visibility/checkVisibility.ts
+++ src/auth/visibility/checkVisibility.ts
@@ -2,8 +2,8 @@ import type { MembershipFiltered } from '@/services/groups/memberships/types'
 import type { VisibilityMatrix } from '@/services/visibility/types'
 
 
-export function checkVisibility(memberships: MembershipFiltered[], visibility: VisibilityMatrix) {
-    visibility.requirements.every(
+export function checkVisibility(memberships: MembershipFiltered[], visibility: VisibilityMatrix): boolean {
+    return visibility.requirements.every(
         requirement => requirement.conditions.some(
             condition => {
                 if (condition.type === 'ACTIVE') {
