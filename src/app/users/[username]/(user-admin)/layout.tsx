90d181b8aedb0d189af00c63e912fa1a8ea6a5c1
ablerto <albertroed@hotmail.com>
2026-01-19 20:19:34 +0100

    feat: add subPage navBar for settings page
    Still missing some styling and choises for computers.
    Looks fine for non-admins in the ranges: {[200px,900px], [1200px,-]}

--- src/app/users/[username]/(user-admin)/layout.tsx
+++ src/app/users/[username]/(user-admin)/layout.tsx
@@ -8,6 +8,9 @@ import { notFound } from 'next/navigation'
 import Link from 'next/link'
 import type { ReactNode } from 'react'
 import type { PropTypes } from '@/app/users/[username]/page'
+import SubPageNavBar from '@/components/NavBar/SideNavBar/SubPageNavBar'
+import SubPageNavBarItem from '@/components/NavBar/SideNavBar/SubPageNavBarItem'
+import { faCircleDot, faCog, faKey, faPaperPlane, faUser } from '@fortawesome/free-solid-svg-icons'
 
 export default async function UserAdmin({ children, params }: PropTypes & { children: ReactNode }) {
     const session = await Session.fromNextAuth()
@@ -19,16 +22,20 @@ export default async function UserAdmin({ children, params }: PropTypes & { chil
     const { user } = unwrapActionReturn(await readUserProfileAction({ params: { username } }))
     return (
         <PageWrapper title={`Innstillinger for ${user.firstname} ${user.lastname}`}>
-            <Link href={`/users/${username}`} className={styles.toProfile}>
-                Til Profilsiden
-            </Link>
             <div className={styles.userAdminLayout}>
                 <i>Bruker Id: {user.id}</i> <br />
                 <i>Brukernavn: {user.username}</i>
                 <main>
                     {children}
                 </main>
-                <Nav username={username} />
+                {/* <Nav username={username} /> */}
+                <SubPageNavBar>
+                    <SubPageNavBarItem icon={faUser} href={`/users/${username}`}>Profil</SubPageNavBarItem>
+                    <SubPageNavBarItem icon={faCircleDot} href={`/user/${username}/dots`}>Prikker</SubPageNavBarItem>
+                    <SubPageNavBarItem icon={faPaperPlane} href={`/users/${username}/notifications`}>Notifikasjoner</SubPageNavBarItem>
+                    <SubPageNavBarItem icon={faKey} href={`/users/${username}/permissions`}>Tilganger</SubPageNavBarItem>
+                    <SubPageNavBarItem icon={faCog} href={`/users/${username}/settings`}>Innstillinger</SubPageNavBarItem>
+                </SubPageNavBar>
             </div>
         </PageWrapper>
     )
