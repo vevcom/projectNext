57ef5125b080369681758ef455f65654d1e48c7c
Paulius Juzenas <pauliusj@stud.ntnu.no>
2026-01-21 11:55:43 +0100

    fix: add missing account from sidebar

--- src/app/users/[username]/(user-admin)/layout.tsx
+++ src/app/users/[username]/(user-admin)/layout.tsx
@@ -1,16 +1,14 @@
 import styles from './layout.module.scss'
-import Nav from './Nav'
 import { readUserProfileAction } from '@/services/users/actions'
 import { unwrapActionReturn } from '@/app/redirectToErrorPage'
 import { ServerSession } from '@/auth/session/ServerSession'
 import PageWrapper from '@/components/PageWrapper/PageWrapper'
 import { notFound } from 'next/navigation'
-import Link from 'next/link'
 import type { ReactNode } from 'react'
 import type { PropTypes } from '@/app/users/[username]/page'
 import SubPageNavBar from '@/components/NavBar/SideNavBar/SubPageNavBar'
 import SubPageNavBarItem from '@/components/NavBar/SideNavBar/SubPageNavBarItem'
-import { faCircleDot, faCog, faKey, faPaperPlane, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCircleDot, faCog, faKey, faMoneyBill1Wave, faPaperPlane, faUser } from '@fortawesome/free-solid-svg-icons'
 
 export default async function UserAdmin({ children, params }: PropTypes & { children: ReactNode }) {
     const session = await ServerSession.fromNextAuth()
@@ -33,6 +31,7 @@ export default async function UserAdmin({ children, params }: PropTypes & { chil
                     <SubPageNavBarItem icon={faUser} href={`/users/${username}`}>Profil</SubPageNavBarItem>
                     <SubPageNavBarItem icon={faCircleDot} href={`/user/${username}/dots`}>Prikker</SubPageNavBarItem>
                     <SubPageNavBarItem icon={faPaperPlane} href={`/users/${username}/notifications`}>Notifikasjoner</SubPageNavBarItem>
+                    <SubPageNavBarItem icon={faMoneyBill1Wave} href={`/users/${username}/konto`}>Konto</SubPageNavBarItem>
                     <SubPageNavBarItem icon={faKey} href={`/users/${username}/permissions`}>Tilganger</SubPageNavBarItem>
                     <SubPageNavBarItem icon={faCog} href={`/users/${username}/settings`}>Innstillinger</SubPageNavBarItem>
                 </SubPageNavBar>
