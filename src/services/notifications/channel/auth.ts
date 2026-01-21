dfb01fca20961b03a82d5faab0e346b2c114b1f3
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2026-01-21 17:20:47 +0100

    chore: Remove permission NOTIFICATION_CHANNEL_READ

--- src/services/notifications/channel/auth.ts
+++ src/services/notifications/channel/auth.ts
@@ -1,10 +1,11 @@
+import { RequireNothing } from '@/auth/authorizer/RequireNothing'
 import { RequirePermission } from '@/auth/authorizer/RequirePermission'
 import '@pn-server-only'
 
 export const notificationChannelAuth = {
     create: RequirePermission.staticFields({ permission: 'NOTIFICATION_CHANNEL_CREATE' }),
-    readMany: RequirePermission.staticFields({ permission: 'NOTIFICATION_CHANNEL_READ' }),
-    readDefault: RequirePermission.staticFields({ permission: 'NOTIFICATION_CHANNEL_READ' }),
+    readMany: RequireNothing.staticFields({}),
+    readDefault: RequireNothing.staticFields({}),
     update: RequirePermission.staticFields({ permission: 'NOTIFICATION_CHANNEL_UPDATE' }),
     destroy: RequirePermission.staticFields({ permission: 'NOTIFICATION_CHANNEL_UPDATE' }),
 }
