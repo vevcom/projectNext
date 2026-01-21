2f8d49959b32aa8864559607af3616a1c6b60924
Paulius Juzenas <pauliusj@stud.ntnu.no>
2026-01-21 14:30:22 +0100

    fix: prod stripe publishable key inlining

--- src/app/_components/Stripe/StripeProvider.tsx
+++ src/app/_components/Stripe/StripeProvider.tsx
@@ -5,11 +5,17 @@ import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import type { ReactNode } from 'react'
 
-if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+// To avoid inlining of the publishable key during build time (where the 
+// environment variable is not set) we use a variable for the key name.
+// See the non inlined examples here: 
+// https://nextjs.org/docs/app/guides/environment-variables#bundling-environment-variables-for-the-browser
+const NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY = 'NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY'
+
+if (!process.env[NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY]) {
     throw new Error('Stripe publishable key not set')
 }
 
-const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+const stripe = loadStripe(process.env[NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY]!)
 
 type Props = {
     children?: ReactNode,
