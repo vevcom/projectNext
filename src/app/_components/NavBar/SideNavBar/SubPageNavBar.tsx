4fe445f40e4ff8c764a9f4eb5bd8d44313e2c1b9
Vegard <vegard@steintre.com>
2026-04-14 17:03:37 +0200

    fix: review comments

--- src/app/_components/NavBar/SideNavBar/SubPageNavBar.tsx
+++ src/app/_components/NavBar/SideNavBar/SubPageNavBar.tsx
@@ -1,25 +1,25 @@
 'use client'
-import type { ReactNode } from 'react'
 import styles from './SubPageNavBar.module.scss'
-import type { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
+import type { IconProp } from '@fortawesome/fontawesome-svg-core'
+import type { ReactNode } from 'react'
 
-interface NavBarItemProps {
+type SubNavBarItemProps = {
     children?: ReactNode,
     icon?: IconProp,
     href: string
 }
 
-export function SubPageNavBarItem({ children, icon, href }:NavBarItemProps) {
+export function SubPageNavBarItem({ children, icon, href }:SubNavBarItemProps) {
     const pathname = usePathname()
     const page = pathname.split('/').pop()
     const thisPage = href?.split('/').pop()
 
     return (
         <div className={styles.linkContainer}>
-            <Link href={new URL(href)} className={styles.link}>
+            <Link href={href} className={styles.link}>
                 <div className={`${styles.container} ${page === thisPage ? styles.selected : styles.notSelected}`}>
                     {icon && <FontAwesomeIcon icon={icon} className={styles.icon} />}
                     <p className={styles.text}>{children}</p>
