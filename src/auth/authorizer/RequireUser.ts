3a49801145f9de4ab5f4dd5d388de7e3ece3a47a
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2026-08-17 11:17:28 +0200

    style: linting

--- src/auth/authorizer/RequireUser.ts
+++ src/auth/authorizer/RequireUser.ts
@@ -7,8 +7,8 @@ export const RequireUser = AuthorizerFactory<
     Record<string, never>,
     Record<string, never>,
     'USER_REQUIERED_FOR_AUTHORIZED'
->(({ session }) => 
+>(({ session }) =>
     (session.user ?
-    { success: true, session } :
-    { success: false, session, errorMessage: 'Du må være innlogget for å få tilgang' }
-))
+        { success: true, session } :
+        { success: false, session, errorMessage: 'Du må være innlogget for å få tilgang' }
+    ))
